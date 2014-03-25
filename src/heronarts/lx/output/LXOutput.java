95748ee09ae9084f37516488daf557b893484d17
Mark Slee <mcslee@gmail.com>
2014-03-24 18:16:54 -0700

    Clean up output code a little

--- src/heronarts/lx/output/LXOutput.java
+++ src/heronarts/lx/output/LXOutput.java
@@ -53,8 +53,19 @@ public abstract class LXOutput {
      */
     public final DiscreteParameter mode = new DiscreteParameter("MODE", 3);
     
+    /**
+     * Normal output mode, colors are sent according to animation
+     */
     public final static int MODE_NORMAL = 0;
+    
+    /**
+     * White output mode, all pixels are sent full white
+     */
     public final static int MODE_WHITE = 1;
+    
+    /**
+     * Off output mode, all pixels are sent off
+     */
     public final static int MODE_OFF = 2;
     
     /**
@@ -130,36 +141,44 @@ public abstract class LXOutput {
             int[] colorsToSend;
             switch (this.mode.getValuei()) {
             case MODE_WHITE:
+                int white = LX.hsb(0, 0, 100*this.brightness.getValuef());
+                for (int i = 0; i < this.allWhite.length; ++i) {
+                    this.allWhite[i] = white;
+                }
                 colorsToSend = this.allWhite;
                 break;
+            
             case MODE_OFF:
                 colorsToSend = this.allOff;
                 break;
+            
             default:
             case MODE_NORMAL:
                 colorsToSend = colors;
+                int gamma = this.gammaCorrection.getValuei();
+                double brt = this.brightness.getValuef(); 
+                if (gamma > 0 || brt < 1) {
+                    int r, g, b, rgb;
+                    for (int i = 0; i < colorsToSend.length; ++i) {
+                        rgb = colorsToSend[i];
+                        r = (rgb >> 16) & 0xff;
+                        g = (rgb >> 8) & 0xff;
+                        b = rgb & 0xff;        
+                        Color.RGBtoHSB(r, g, b, this.hsb);
+                        float scaleBrightness = hsb[2];
+                        for (int x = 0; x < gamma; ++x) {
+                            scaleBrightness *= hsb[2];
+                        }
+                        scaleBrightness *= brt;
+                        this.outputColors[i] = Color.HSBtoRGB(hsb[0], hsb[1], scaleBrightness);
+                    }
+                    colorsToSend = this.outputColors;
+                }
                 break;
             }
-            int gamma = this.gammaCorrection.getValuei();
-            double brt = this.brightness.getValuef(); 
-            if (gamma > 0 || brt < 1) {
-                int r, g, b, rgb;
-                for (int i = 0; i < colorsToSend.length; ++i) {
-                    rgb = colorsToSend[i];
-                    r = (rgb >> 16) & 0xff;
-                    g = (rgb >> 8) & 0xff;
-                    b = rgb & 0xff;        
-                    Color.RGBtoHSB(r, g, b, this.hsb);
-                    float scaleBrightness = hsb[2];
-                    for (int x = 0; x < gamma; ++x) {
-                        scaleBrightness *= hsb[2];
-                    }
-                    scaleBrightness *= brt;
-                    this.outputColors[i] = Color.HSBtoRGB(hsb[0], hsb[1], scaleBrightness);
-                }
-                colorsToSend = this.outputColors;
-            }
+            
             this.onSend(colorsToSend);
+            
             for (LXOutput child : this.children) {
                 child.send(colorsToSend);
             }
