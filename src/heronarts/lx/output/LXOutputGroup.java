e0ae641c07340419554c8a6feebaebd4f6b27253
Mark Slee <mcslee@MacBook-Pro.local>
2015-07-22 16:39:38 -0700

    Output group

---
+++ src/heronarts/lx/output/LXOutputGroup.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2015- Mark C. Slee, Heron Arts LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Mark C. Slee <mark@heronarts.com>
+ */
+
+package heronarts.lx.output;
+
+import heronarts.lx.LX;
+
+/**
+ * Simple concrete output class which does nothing but group its children.
+ */
+public class LXOutputGroup extends LXOutput {
+
+    protected LXOutputGroup(LX lx) {
+        super(lx);
+    }
+
+    @Override
+    protected void onSend(int[] colors) {
+        // Do nothing
+    }
+
+}
