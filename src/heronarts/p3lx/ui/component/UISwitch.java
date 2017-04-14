e61182be74246248a972d3fc20499fbd547d7a69
Mark Slee <mcslee@gmail.com>
2017-04-14 14:12:34 -0700

    Switch follow Momentary policy of assigned parameter

--- src/heronarts/p3lx/ui/component/UISwitch.java
+++ src/heronarts/p3lx/ui/component/UISwitch.java
@@ -5,12 +5,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -56,7 +56,9 @@ public class UISwitch extends UIParameterControl implements UIFocus {
         if (!(parameter instanceof BooleanParameter)) {
             throw new IllegalArgumentException("UISwitch may only take BooleanParameter");
         }
-        return super.setParameter(parameter);
+        super.setParameter(parameter);
+        setMomentary(getBooleanParameter().getMode() == BooleanParameter.Mode.MOMENTARY);
+        return this;
     }
 
     private BooleanParameter getBooleanParameter() {
