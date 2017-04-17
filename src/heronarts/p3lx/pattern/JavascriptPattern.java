c53afd1bc769eea50ff7360b8b50dcdcaa7d5d05
Mark Slee <mcslee@gmail.com>
2017-04-17 16:29:10 -0700

    Knobs for Javascript pattern parameters

--- src/heronarts/p3lx/pattern/JavascriptPattern.java
+++ src/heronarts/p3lx/pattern/JavascriptPattern.java
@@ -23,9 +23,17 @@ package heronarts.p3lx.pattern;
 import java.io.IOException;
 
 import heronarts.lx.LX;
+import heronarts.lx.parameter.BoundedParameter;
+import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.LXListenableNormalizedParameter;
+import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.script.LXScriptPattern;
 import heronarts.p3lx.ui.UI;
+import heronarts.p3lx.ui.UI2dComponent;
+import heronarts.p3lx.ui.UI2dContainer;
+import heronarts.p3lx.ui.UIObject;
 import heronarts.p3lx.ui.component.UIButton;
+import heronarts.p3lx.ui.component.UIKnob;
 import heronarts.p3lx.ui.component.UITextBox;
 import heronarts.p3lx.ui.studio.device.UIPattern;
 import heronarts.p3lx.ui.studio.device.UIPatternControl;
@@ -33,20 +41,24 @@ import processing.core.PConstants;
 
 public class JavascriptPattern extends LXScriptPattern implements UIPattern {
 
+    private static final int MIN_WIDTH = 120;
+
+    private UI2dContainer knobs;
+
     public JavascriptPattern(LX lx) {
         super(lx);
     }
 
     @Override
-    public void buildControlUI(UI ui, UIPatternControl container) {
-        container.setWidth(120);
+    public void buildControlUI(UI ui, final UIPatternControl container) {
+        container.setContentWidth(MIN_WIDTH);
 
-        new UITextBox(0, 0, container.getContentWidth() - 18, 16)
+        new UITextBox(0, 0, container.getContentWidth() - 22, 20)
         .setParameter(this.scriptPath)
-        .setTextAlignment(PConstants.LEFT)
+        .setTextAlignment(PConstants.CENTER)
         .addToContainer(container);
 
-        new UIButton(container.getContentWidth() - 16, 0, 16, 16) {
+        new UIButton(container.getContentWidth() - 20, 0, 20, 20) {
             @Override
             public void onToggle(boolean on) {
                 if (on) {
@@ -58,7 +70,7 @@ public class JavascriptPattern extends LXScriptPattern implements UIPattern {
         .setMomentary(true)
         .addToContainer(container);
 
-        new UIButton(0, 20, container.getContentWidth(), 16) {
+        new UIButton(0, 24, container.getContentWidth(), 16) {
             @Override
             public void onToggle(boolean on) {
                 if (on) {
@@ -74,6 +86,38 @@ public class JavascriptPattern extends LXScriptPattern implements UIPattern {
         .setMomentary(true)
         .addToContainer(container);
 
+        this.knobs = new UI2dContainer(0, 44, container.getContentWidth(), container.getContentHeight() - 44) {
+            @Override
+            protected void onResize() {
+                container.setContentWidth(getWidth());
+            }
+        };
+        knobs.setLayout(UI2dContainer.Layout.VERTICAL_GRID);
+        knobs.setPadding(2, 0, 0, 0);
+        knobs.setChildMargin(2, 4);
+        knobs.setMinWidth(MIN_WIDTH);
+        knobs.addToContainer(container);
+
+        resetKnobs();
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        resetKnobs();
+    }
+
+    private void resetKnobs() {
+        if (this.knobs != null) {
+            for (UIObject child : this.knobs) {
+                ((UI2dComponent) child).removeFromContainer();
+            }
+            for (LXParameter p : this.jsParameters) {
+                if (p instanceof BoundedParameter || p instanceof DiscreteParameter) {
+                    new UIKnob((LXListenableNormalizedParameter) p).addToContainer(this.knobs);
+                }
+            }
+        }
     }
 
 }
