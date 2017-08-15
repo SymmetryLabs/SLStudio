c2612df25d1ecbde075d6b97deaaf6aaf6f96f0d
Mark Slee <mcslee@gmail.com>
2017-08-15 14:04:07 -0700

    Fix little bitshift bug in screen code

--- src/heronarts/lx/blend/ScreenBlend.java
+++ src/heronarts/lx/blend/ScreenBlend.java
@@ -5,12 +5,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -30,6 +30,10 @@ public class ScreenBlend extends LXBlend {
 
     @Override
     public void blend(int[] dst, int[] src, double alpha, int[] output) {
+        screen(dst, src, alpha, output);
+    }
+
+    public static void screen(int[] dst, int[] src, double alpha, int[] output) {
         int alphaAdjust = (int) (alpha * 0x100);
         for (int i = 0; i < src.length; ++i) {
             int a = (((src[i] >>> ALPHA_SHIFT) * alphaAdjust) >> 8) & 0xff;
@@ -47,7 +51,7 @@ public class ScreenBlend extends LXBlend {
                     (src[i] & R_MASK) * (dstR + 1) |
                     (src[i] & B_MASK) * (dstB + 1)
                 ) >>> 8 & RB_MASK;
-            int gnSub = srcGn * (dstGn + 0x100) >> G_SHIFT & G_MASK;
+            int gnSub = srcGn * (dstGn + 0x100) >> 16 & G_MASK;
 
             output[i] = min((dst[i] >>> ALPHA_SHIFT) + a, 0xff) << ALPHA_SHIFT |
                 (dstRb * dstAlpha + (dstRb + (src[i] & RB_MASK) - rbSub) * srcAlpha) >>> 8 & RB_MASK |
