8da420fc0b82fd7a362e76b5ade12763a8f036cd
Hike Danakian <hike@hdanak.com>
2017-12-03 17:04:58 -0800

    Add LayeredRenderer to handle a larger number of particles.

--- SLStudio/ParticlePattern.java
+++ SLStudio/ParticlePattern.java
@@ -7,12 +7,14 @@ import java.util.HashMap;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import java.util.function.Consumer;
-import java.nio.IntBuffer;
+
+import org.apache.commons.math3.util.FastMath;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXPattern;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.model.LXModel;
+import heronarts.lx.model.LXFixture;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.BoundedParameter;
 import heronarts.lx.parameter.BooleanParameter;
@@ -20,6 +22,7 @@ import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.parameter.LXParameterListener;
 import heronarts.lx.color.LXColor;
 
+import com.symmetrylabs.util.LayeredRenderer;
 import com.symmetrylabs.util.ModelIndex;
 import com.symmetrylabs.util.OctreeModelIndex;
 
@@ -27,7 +30,7 @@ public abstract class ParticlePattern extends LXPattern {
     private static final int DEFAULT_PARTICLE_COUNT = 10;
     private final double SQRT_2PI = Math.sqrt(2 * Math.PI);
 
-    public final BoundedParameter particleCount = new BoundedParameter("count", 0, 0, 200);
+    public final BoundedParameter particleCount = new BoundedParameter("count", 0, 0, 1000);
     public final CompoundParameter kernelSize = new CompoundParameter("size", 100, 0, 400);
     public final BooleanParameter flattenZ = new BooleanParameter("flattenZ", true);
 
@@ -37,8 +40,7 @@ public abstract class ParticlePattern extends LXPattern {
     protected List<Particle> particles = new ArrayList<>();
     protected ModelIndex modelIndex;
 
-    private Map<Particle, float[]> brightnessLayers = new HashMap<Particle, float[]>();
-    private float[] brightnessBuffer;
+    private ParticleRenderer renderer;
 
     public ParticlePattern(LX lx) {
         this(lx, DEFAULT_PARTICLE_COUNT);
@@ -47,7 +49,7 @@ public abstract class ParticlePattern extends LXPattern {
     public ParticlePattern(LX lx, int numParticles) {
         super(lx);
 
-        brightnessBuffer = new float[colors.length];
+        renderer = new ParticleRenderer();
 
         addParameter(particleCount);
         addParameter(kernelSize);
@@ -60,19 +62,15 @@ public abstract class ParticlePattern extends LXPattern {
 
         particleCount.addListener(new LXParameterListener() {
             public void onParameterChanged(LXParameter particleCount) {
-                synchronized (brightnessLayers) {
-                    int numParticles = (int)particleCount.getValue();
-                    while (particles.size() > numParticles) {
-                        Particle p = particles.remove(particles.size() - 1);
-                        brightnessLayers.remove(p);
-                    }
+                int numParticles = (int)particleCount.getValue();
+                while (particles.size() > numParticles) {
+                    particles.remove(particles.size() - 1);
+                }
 
-                    for (int i = particles.size(); i < numParticles; ++i) {
-                        Particle p = new Particle();
-                        brightnessLayers.put(p, new float[brightnessBuffer.length]);
-                        initParticle(p);
-                        particles.add(p);
-                    }
+                for (int i = particles.size(); i < numParticles; ++i) {
+                    Particle p = new Particle();
+                    initParticle(p);
+                    particles.add(p);
                 }
             }
         });
@@ -93,12 +91,12 @@ public abstract class ParticlePattern extends LXPattern {
     protected float kernel(float d, float s) {
         double stddev = s / 4f;
         double peak = 1.0f / (2.5f * stddev);
-        return (float)(Math.exp(-(d * d) / (2 * stddev * stddev))
+        return (float)(FastMath.exp(-(d * d) / (2 * stddev * stddev))
                             / (stddev * SQRT_2PI) / peak);
     }
 
     protected float kernel(float x, float y, float z, float s) {
-        return kernel((float)Math.sqrt(x * x + y * y + z * z), s);
+        return kernel((float)FastMath.sqrt(x * x + y * y + z * z), s);
     }
 
     protected void initParticle(Particle p) { }
@@ -115,60 +113,25 @@ public abstract class ParticlePattern extends LXPattern {
 
     @Override
     public void run(double deltaMs) {
-        //System.out.println(deltaMs);
-
         simulate(deltaMs);
-
-        final List<Particle> particleList = new ArrayList<>(particles);
-        final Map<Particle, float[]> layersMap = new HashMap<>(brightnessLayers);
-        final List<float[]> layersList = new ArrayList<>(particles.size());
-
-        synchronized (brightnessLayers) {
-            for (Particle particle : particleList) {
-                if (brightnessLayers.containsKey(particle)) {
-                    layersList.add(brightnessLayers.get(particle));
-                }
-            }
-        }
-
-        // TODO: make per-particle layer sparse
-        particleList.parallelStream().forEach(new Consumer<Particle>() {
-            public void accept(Particle particle) {
-                if (layersMap.containsKey(particle)) {
-                    renderParticle(particle, layersMap.get(particle));
-                }
-            }
-        });
-
-        Arrays.asList(model.points).parallelStream().forEach(new Consumer<LXPoint>() {
-            public void accept(LXPoint point) {
-                float b = 0;
-                for (float[] layer : layersList) {
-                    b += layer[point.index];
-                }
-
-                brightnessBuffer[point.index] = b;
-            }
-        });
-
-        double h = hue.getValue();
-        double s = saturation.getValue();
-        for (LXPoint point : model.points) {
-            float b = Math.min(brightnessBuffer[point.index] * 100, 100);
-            colors[point.index] = LXColor.hsb(h, s, b);
-        }
+        renderer.run(deltaMs);
     }
 
-    protected void renderParticle(Particle particle, float[] brightnessLayer) {
-        Arrays.fill(brightnessLayer, 0);
+    protected void renderParticle(Particle particle, int[] layer) {
+        Arrays.fill(layer, 0);
 
         LXPoint pp = particle.toPointInModel(lx.model);
         float withinDist = particle.size * kernelSize.getValuef();
         List<LXPoint> nearbyPoints = modelIndex.pointsWithin(pp, withinDist);
 
+        double h = hue.getValue();
+        double s = saturation.getValue();
+
         final boolean flattening = flattenZ.isOn();
         for (LXPoint p : nearbyPoints) {
-            brightnessLayer[p.index] = kernel(pp.x - p.x, pp.y - p.y, flattening ? 0 : pp.z - p.z, withinDist);
+            float b = kernel(pp.x - p.x, pp.y - p.y, flattening ? 0 : pp.z - p.z, withinDist);
+            int c = LXColor.hsb(h, s, FastMath.min(b * 100, 100));
+            layer[p.index] = LXColor.blend(layer[p.index], c, LXColor.Blend.ADD);
         }
     }
 
@@ -177,11 +140,33 @@ public abstract class ParticlePattern extends LXPattern {
         public float[] vel = new float[3];
         public float size = 1;
 
+        private LXPoint point = new LXPoint(0, 0, 0);
+
         LXPoint toPointInModel(LXModel model) {
             float x = model.cx + pos[0] * model.xRange / 2f;
             float y = model.cy + pos[1] * model.yRange / 2f;
             float z = model.cz + pos[2] * model.zRange / 2f;
-            return new LXPoint(x, y, z);
+            point.update(x, y, z);
+            return point;
+        }
+    }
+
+    private class ParticleRenderer extends LayeredRenderer {
+        public ParticleRenderer() {
+            super(lx.model, colors);
+        }
+
+        @Override
+        protected void render(double deltaMs, List<LXPoint> points, int[] layer, int index, int numThreads) {
+            List<Particle> particleList = new ArrayList<>(particles);
+
+            int startInclusive = particleList.size() * index / numThreads;
+            int endExclusive = particleList.size() * (index + 1) / numThreads;
+
+            for (int i = startInclusive; i < endExclusive; ++i) {
+                Particle particle = particleList.get(i);
+                renderParticle(particle, layer);
+            }
         }
     }
 }
