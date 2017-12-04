cabef18aa8d85c46b8e743eb7a2eff5e0a5a522d
Hike Danakian <hike@hdanak.com>
2017-12-03 19:31:35 -0800

    Fix bounds of particle pattern.

--- SLStudio/ParticlePattern.java
+++ SLStudio/ParticlePattern.java
@@ -2,6 +2,7 @@ package com.symmetrylabs.pattern;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Vector;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Arrays;
@@ -27,7 +28,6 @@ import com.symmetrylabs.util.ModelIndex;
 import com.symmetrylabs.util.OctreeModelIndex;
 
 public abstract class ParticlePattern extends LXPattern {
-    private static final int DEFAULT_PARTICLE_COUNT = 10;
     private final double SQRT_2PI = Math.sqrt(2 * Math.PI);
 
     public final BoundedParameter particleCount = new BoundedParameter("count", 0, 0, 1000);
@@ -37,16 +37,12 @@ public abstract class ParticlePattern extends LXPattern {
     public final CompoundParameter hue = new CompoundParameter("hue", 0, 0, 360);
     public final CompoundParameter saturation = new CompoundParameter("saturation", 30, 0, 100);
 
-    protected List<Particle> particles = new ArrayList<>();
+    protected List<Particle> particles = new Vector<>();
     protected ModelIndex modelIndex;
 
     private ParticleRenderer renderer;
 
     public ParticlePattern(LX lx) {
-        this(lx, DEFAULT_PARTICLE_COUNT);
-    }
-
-    public ParticlePattern(LX lx, int numParticles) {
         super(lx);
 
         renderer = new ParticleRenderer();
@@ -80,8 +76,6 @@ public abstract class ParticlePattern extends LXPattern {
                 ParticlePattern.this.modelIndex = createModelIndex();
             }
         });
-
-        particleCount.setValue(numParticles);
     }
 
     private ModelIndex createModelIndex() {
@@ -102,15 +96,6 @@ public abstract class ParticlePattern extends LXPattern {
     protected void initParticle(Particle p) { }
     protected abstract void simulate(double deltaMs);
 
-    /** Only needed until we can do IntRange.range in Processing */
-    private Stream intRangeStream(final int startInclusive, final int endExclusive) {
-        List<Integer> range = new ArrayList<>(endExclusive - startInclusive);
-        for (int i = 0; i < (endExclusive - startInclusive); ++i) {
-            range.add(i + startInclusive);
-        }
-        return range.parallelStream();
-    }
-
     @Override
     public void run(double deltaMs) {
         simulate(deltaMs);
@@ -120,7 +105,7 @@ public abstract class ParticlePattern extends LXPattern {
     protected void renderParticle(Particle particle, int[] layer) {
         Arrays.fill(layer, 0);
 
-        LXPoint pp = particle.toPointInModel(lx.model);
+        LXPoint pp = particle.toPointInModel();
         float withinDist = particle.size * kernelSize.getValuef();
         List<LXPoint> nearbyPoints = modelIndex.pointsWithin(pp, withinDist);
 
@@ -142,10 +127,10 @@ public abstract class ParticlePattern extends LXPattern {
 
         private LXPoint point = new LXPoint(0, 0, 0);
 
-        LXPoint toPointInModel(LXModel model) {
-            float x = model.cx + pos[0] * model.xRange / 2f;
-            float y = model.cy + pos[1] * model.yRange / 2f;
-            float z = model.cz + pos[2] * model.zRange / 2f;
+        LXPoint toPointInModel() {
+            float x = lx.model.xMin + pos[0] * lx.model.xRange;
+            float y = lx.model.yMin + pos[1] * lx.model.yRange;
+            float z = lx.model.zMin + pos[2] * lx.model.zRange;
             point.update(x, y, z);
             return point;
         }
