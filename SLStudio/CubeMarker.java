dcf0c191823f464e25527d9c1fb72f445cd30d88
Ka-Ping Yee <zestyping@gmail.com>
2017-12-05 13:15:51 -0500

    Reorganize FlockWave parameters.

--- SLStudio/CubeMarker.java
+++ SLStudio/CubeMarker.java
@@ -5,13 +5,17 @@ import processing.core.PVector;
 
 public class CubeMarker implements Marker {
     PVector pos;
-    float size;
+    PVector size;
     int rgb;
 
-    public CubeMarker(PVector pos, float size, int rgb) {
+    public CubeMarker(PVector pos, PVector size, int rgb) {
         this.pos = pos;
         this.size = size;
         this.rgb = rgb;
+    }
+
+    public CubeMarker(PVector pos, float size, int rgb) {
+        this(pos, new PVector(size, size, size), rgb);
     }
 
     public void draw(PGraphics pg) {
@@ -22,9 +26,9 @@ public class CubeMarker implements Marker {
         pg.stroke((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff);
         for (int d = -1; d < 2; d += 2) {
             for (int e = -1; e < 2; e += 2) {
-                pg.line(x - size, y + d*size, z + e*size, x + size, y + d*size, z + e*size);
-                pg.line(x + d*size, y - size, z + e*size, x + d*size, y + size, z + e*size);
-                pg.line(x + d*size, y + e*size, z - size, x + d*size, y + e*size, z + size);
+                pg.line(x - size.x, y + d*size.y, z + e*size.z, x + size.x, y + d*size.y, z + e*size.z);
+                pg.line(x + d*size.x, y - size.y, z + e*size.z, x + d*size.x, y + size.y, z + e*size.z);
+                pg.line(x + d*size.x, y + e*size.y, z - size.z, x + d*size.x, y + e*size.y, z + size.z);
             }
         }
     }
