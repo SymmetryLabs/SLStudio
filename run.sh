479406e8ff576079f411f2525daef8a8a907e1ec
Yona Appletree <yona@concentricsky.com>
2017-12-11 02:09:00 -0500

    Removed old processing build scripts

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-command -v processing-java >/dev/null 2>&1 || { echo >&2 "processing-java not found. From Processing IDE, use Tools | Install \"processing-java\" to install"; exit 1; }
-
-processing-java --force --sketch=$(pwd)/SLStudio --output=$(pwd)/build --run
