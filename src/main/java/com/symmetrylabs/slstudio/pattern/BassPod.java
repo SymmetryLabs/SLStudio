8cc95aa0355aa549231f007268d430918f7ad077
Hike Danakian <hike@hdanak.com>
2017-12-29 20:23:23 -0800

    Added PerSunBassPod, doesn't work.

--- src/main/java/com/symmetrylabs/slstudio/pattern/BassPod.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/BassPod.java
@@ -1,5 +1,7 @@
 package com.symmetrylabs.slstudio.pattern;
 
+import java.util.List;
+
 import heronarts.lx.LX;
 import heronarts.lx.audio.GraphicMeter;
 import heronarts.lx.audio.LXAudioInput;
@@ -8,7 +10,7 @@ import heronarts.lx.parameter.CompoundParameter;
 
 import static processing.core.PApplet.*;
 
-public class BassPod extends SunsPattern {
+public class BassPod extends RenderablePattern {
 
     private LXAudioInput audioInput = lx.engine.audio.getInput();
     private GraphicMeter eq = new GraphicMeter(audioInput);
@@ -17,7 +19,9 @@ public class BassPod extends SunsPattern {
 
     public BassPod(LX lx) {
         super(lx);
+
         eq.start();
+
         addParameter(clr);
 //        addParameter(eq.gain);   //to-do can't add these, causes null pointer exception
 //        addParameter(eq.range);
@@ -27,35 +31,33 @@ public class BassPod extends SunsPattern {
         addModulator(eq).start();
     }
 
-    public void onActive() {
+    @Override
+    public void onUIStart() {
         eq.range.setValue(36);
         eq.release.setValue(300);
         eq.gain.setValue(-6);
         eq.slope.setValue(6);
     }
 
-    public void run(double deltaMs) {
+    @Override
+    public void render(double deltaMs, List<LXPoint> points, int[] layer) {
         final float bassLevel = eq.getAveragef(0, 5);
         final float satBase = bassLevel * 480 * clr.getValuef();
 
-        model.forEachPoint((start, end) -> {
-            for (int pi=start; pi<end; pi++) {
-                LXPoint p = model.points[pi];
-
-                int avgIndex = (int) constrain(1 + abs(p.x - model.cx) / (model.cx) * (eq.numBands - 5), 0, eq.numBands - 5);
-                float value = 0;
-                for (int i = avgIndex; i < avgIndex + 5; ++i) {
-                    value += eq.getBandf(i);
-                }
-                value /= 5.;
-
-                float b = constrain(8 * (value * model.yMax - abs(p.y - model.yMax / 2f)), 0, 100);
-                colors[p.index] = lx.hsb(
-                    palette.getHuef() + abs(p.y - model.cy) + abs(p.x - model.cx),
-                    constrain(satBase - .6f * dist(p.x, p.y, model.cx, model.cy), 0, 100),
-                    b
-                );
+        points.parallelStream().forEach(p -> {
+            int avgIndex = (int) constrain(1 + abs(p.x - model.cx) / (model.cx) * (eq.numBands - 5), 0, eq.numBands - 5);
+            float value = 0;
+            for (int i = avgIndex; i < avgIndex + 5; ++i) {
+                value += eq.getBandf(i);
             }
+            value /= 5.;
+
+            float b = constrain(8 * (value * model.yMax - abs(p.y - model.yMax / 2f)), 0, 100);
+            layer[p.index] = lx.hsb(
+                palette.getHuef() + abs(p.y - model.cy) + abs(p.x - model.cx),
+                constrain(satBase - .6f * dist(p.x, p.y, model.cx, model.cy), 0, 100),
+                b
+            );
         });
     }
 }
