d9084938ed1626215af4eac2a6677e2df67d0683
Kyle Fleming <kyle@kylefleming.net>
2017-12-30 04:42:02 -0500

    Allow event handling in patterns

--- src/main/java/com/symmetrylabs/slstudio/pattern/SLPattern.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/SLPattern.java
@@ -7,8 +7,10 @@ import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.DiscreteParameter;
 import heronarts.lx.parameter.LXParameter;
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
 
- /* Non-Patterns */
+/* Non-Patterns */
 public abstract class SLPattern extends LXPattern {
 
     protected final SLStudioLX lx;
@@ -39,4 +41,30 @@ public abstract class SLPattern extends LXPattern {
     protected DiscreteParameter discreteParameter(String name, int value, int min, int max) {
         return addParam(new DiscreteParameter(name, value, min, max));
     }
+
+    public void unconsumeKeyEvent() {
+        this.keyEventConsumed = false;
+    }
+
+    public void consumeKeyEvent() {
+        this.keyEventConsumed = true;
+    }
+
+    public boolean keyEventConsumed() {
+        return this.keyEventConsumed;
+    }
+
+    private boolean keyEventConsumed = false;
+
+    public void onMousePressed(MouseEvent mouseEvent, float mx, float my) {}
+    public void onMouseReleased(MouseEvent mouseEvent, float mx, float my) {}
+    public void onMouseClicked(MouseEvent mouseEvent, float mx, float my) {}
+    public void onMouseDragged(MouseEvent mouseEvent, float mx, float my, float dx, float dy) {}
+    public void onMouseMoved(MouseEvent mouseEvent, float mx, float my) {}
+    public void onMouseOver(MouseEvent mouseEvent) {}
+    public void onMouseOut(MouseEvent mouseEvent) {}
+    public void onMouseWheel(MouseEvent mouseEvent, float mx, float my, float delta) {}
+    public void onKeyPressed(KeyEvent keyEvent, char keyChar, int keyCode) {}
+    public void onKeyReleased(KeyEvent keyEvent, char keyChar, int keyCode) {}
+    public void onKeyTyped(KeyEvent keyEvent, char keyChar, int keyCode) {}
 }
