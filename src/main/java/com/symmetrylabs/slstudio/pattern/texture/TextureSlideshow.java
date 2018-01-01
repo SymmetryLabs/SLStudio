2084ed1107124b73530749d6baa4a96d858c1efb
Hike Danakian <hike@hdanak.com>
2017-12-31 17:15:55 -0800

    Improve responsiveness of TextureSlideshow params.

--- src/main/java/com/symmetrylabs/slstudio/pattern/texture/TextureSlideshow.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/texture/TextureSlideshow.java
@@ -1,7 +1,15 @@
 package com.symmetrylabs.slstudio.pattern.texture;
 
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.awt.image.Kernel;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import javax.imageio.ImageIO;
+
 import org.apache.commons.math3.util.FastMath;
-import processing.core.PImage;
 
 import heronarts.lx.LX;
 import heronarts.lx.color.LXColor;
@@ -9,6 +17,7 @@ import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.SawLFO;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.DiscreteParameter;
 import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.parameter.LXParameterListener;
 
@@ -28,17 +37,26 @@ public abstract class TextureSlideshow extends SunsPattern {
     private final SawLFO lerp = (SawLFO) startModulator(new SawLFO(0, 1, rate));
 
     private int imageIndex = 0;
-    private final PImage[] images;
+    private final BufferedImage[] images;
     private final int[][] imageLayers;
+
+    private UpdateThread updateThread;
 
     public TextureSlideshow(LX lx) {
         super(lx);
 
         String[] paths = getPaths();
-        images = new PImage[paths.length];
+        images = new BufferedImage[paths.length];
         for (int i = 0; i < images.length; ++i) {
-            images[i] = SLStudio.applet.loadImage(paths[i]);
-            images[i].loadPixels();
+            String filePath = SLStudio.applet.dataPath(paths[i]);
+            //System.out.println("Loading image: " + filePath);
+
+            try {
+                images[i] = ImageIO.read(new File(filePath));
+            }
+            catch (IOException e) {
+                System.err.println("Error loading image from '" + filePath + "': " + e.getMessage());
+            }
         }
 
         imageLayers = new int[images.length][colors.length];
@@ -52,21 +70,8 @@ public abstract class TextureSlideshow extends SunsPattern {
         addParameter(enableInterp);
 
         LXParameterListener updateRastersListener = new LXParameterListener() {
-            private boolean inProgress = false;
-
             public void onParameterChanged(LXParameter ignore) {
-                synchronized (this) {
-                    if (inProgress)
-                        return;
-
-                    inProgress = true;
-                }
-
-                updateRasters();
-
-                synchronized (this) {
-                    inProgress = false;
-                }
+                triggerUpdate();
             }
         };
 
@@ -77,20 +82,23 @@ public abstract class TextureSlideshow extends SunsPattern {
         offsetY.addListener(updateRastersListener);
         enableInterp.addListener(updateRastersListener);
 
-        updateRasters();
+        triggerUpdate();
     }
 
     abstract String[] getPaths();
 
-    private int bilinearInterp(PImage image, double px, double py) {
-        int imgOffsX = (int)(offsetX.getValue() * (image.width - 1) + image.width);
-        int imgOffsY = (int)(offsetY.getValue() * (image.height - 1) + image.height);
+    private int bilinearInterp(BufferedImage image, double px, double py) {
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+
+        int imgOffsX = (int)(offsetX.getValue() * (imageWidth - 1) + imageWidth);
+        int imgOffsY = (int)(offsetY.getValue() * (imageHeight - 1) + imageHeight);
 
         double zoomXValue = zoomX.getValue() + 1;
         double zoomYValue = zoomY.getValue() + 1;
 
-        double imgX = px * (image.width - 1) / zoomXValue + imgOffsX;
-        double imgY = py * (image.height - 1) / zoomYValue + imgOffsY;
+        double imgX = px * (imageWidth - 1) / zoomXValue + imgOffsX;
+        double imgY = py * (imageHeight - 1) / zoomYValue + imgOffsY;
 
         int imgXFloor = (int)FastMath.floor(imgX);
         int imgYFloor = (int)FastMath.floor(imgY);
@@ -98,19 +106,19 @@ public abstract class TextureSlideshow extends SunsPattern {
         double xRem = imgX - imgXFloor;
         double yRem = imgY - imgYFloor;
 
-        imgXFloor %= image.width;
-        imgYFloor %= image.height;
+        imgXFloor %= imageWidth;
+        imgYFloor %= imageHeight;
 
         if (!enableInterp.isOn())
-            return image.get(imgXFloor, imgYFloor);
+            return image.getRGB(imgXFloor, imgYFloor);
 
-        int imgXCeil = (int)FastMath.ceil(imgX) % image.width;
-        int imgYCeil = (int)FastMath.ceil(imgY) % image.height;
+        int imgXCeil = (int)FastMath.ceil(imgX) % imageWidth;
+        int imgYCeil = (int)FastMath.ceil(imgY) % imageHeight;
 
-        int q11 = image.get(imgXFloor, imgYFloor);
-        int q12 = image.get(imgXFloor, imgYCeil);
-        int q21 = image.get(imgXCeil, imgYFloor);
-        int q22 = image.get(imgXCeil, imgYCeil);
+        int q11 = image.getRGB(imgXFloor, imgYFloor);
+        int q12 = image.getRGB(imgXFloor, imgYCeil);
+        int q21 = image.getRGB(imgXCeil, imgYFloor);
+        int q22 = image.getRGB(imgXCeil, imgYCeil);
 
         int q1 = LXColor.lerp(q11, q21, xRem);
         int q2 = LXColor.lerp(q12, q22, xRem);
@@ -118,27 +126,66 @@ public abstract class TextureSlideshow extends SunsPattern {
         return LXColor.lerp(q1, q2, yRem);
     }
 
+    private class UpdateThread extends Thread {
+        public boolean needsUpdate = true;
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (TextureSlideshow.this) {
+                    if (needsUpdate) {
+                        //System.out.println("needsUpdate was reset, unsetting");
+                        needsUpdate = false;
+                    }
+                    else {
+                        //System.out.println("updateThread is done, setting to null");
+                        updateThread = null;
+                        return;
+                    }
+                }
+
+                updateRasters();
+            }
+        }
+    }
+
+    private synchronized void triggerUpdate() {
+        if (updateThread != null) {
+            //System.out.println("updateThread found, setting needsUpdate");
+            updateThread.needsUpdate = true;
+        }
+        else {
+            //System.out.println("updateThread is null, creating new");
+            updateThread = new UpdateThread();
+            updateThread.start();
+        }
+    }
+
     private void updateRasters() {
         for (int i = 0; i < images.length; ++i) {
-            PImage image = images[i];
+            BufferedImage image = images[i];
+            if (image == null)
+                continue;
+
             int[] layer = imageLayers[i];
 
-            if (perSun.getValueb()) {
+            final BufferedImage imageFinal = image;
+            if (perSun.isOn()) {
                 for (Sun sun : model.getSuns()) {
-                    for (LXPoint p : sun.points) {
+                    sun.getPoints().parallelStream().forEach(p -> {
                         double px = (p.x - sun.xMin) / sun.xRange;
                         double py = (p.y - sun.yMin) / sun.yRange;
 
-                        layer[p.index] = bilinearInterp(image, px, py);
-                    }
+                        layer[p.index] = bilinearInterp(imageFinal, px, py);
+                    });
                 }
             } else {
-                for (LXPoint p : model.points) {
+                model.getPoints().parallelStream().forEach(p -> {
                     double px = (p.x - model.xMin) / model.xRange;
                     double py = (p.y - model.yMin) / model.yRange;
 
-                    layer[p.index] = bilinearInterp(image, px, py);
-                }
+                    layer[p.index] = bilinearInterp(imageFinal, px, py);
+                });
             }
         }
     }
@@ -155,11 +202,11 @@ public abstract class TextureSlideshow extends SunsPattern {
         int image1Index = imageIndex;
         int image2Index = (imageIndex + 1) % images.length;
 
-        for (LXPoint p : lx.model.points) {
+        model.getPoints().parallelStream().forEach(p -> {
             int c1 = imageLayers[image1Index][p.index];
             int c2 = imageLayers[image2Index][p.index];
 
-            colors[p.index] = LXColor.lerp(c1, c2, lerpValue);
-        }
+            colors[p.index] = c1 == c2 ? c1 : LXColor.lerp(c1, c2, lerpValue);
+        });
     }
 }
