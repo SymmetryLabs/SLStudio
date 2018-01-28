6fe5f195df0c2de6ac7f9fe47e9c5a33ee08a315
Hike Danakian <hike@hdanak.com>
2018-01-28 02:20:37 -0800

    Messing around with LABShift effect.

---
+++ src/main/java/com/symmetrylabs/slstudio/effect/LABShift.java
@@ -0,0 +1,94 @@
+package com.symmetrylabs.slstudio.effect;
+
+import org.apache.commons.math3.util.FastMath;
+
+import heronarts.lx.LX;
+import heronarts.lx.LXEffect;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.color.LXColor;
+import heronarts.lx.parameter.CompoundParameter;
+
+public class LABShift extends LXEffect {
+    public final CompoundParameter L = new CompoundParameter("L", 0, -100, 100);
+    public final CompoundParameter A = new CompoundParameter("A", 0, -100, 100);
+    public final CompoundParameter B = new CompoundParameter("B", 0, -100, 100);
+
+    public LABShift(LX lx) {
+        super(lx);
+
+        addParameter(L);
+        addParameter(A);
+        addParameter(B);
+    }
+
+    final float[] lab = new float[3];
+
+    @Override
+    public void run(double deltaMs, double amount) {
+        for (LXPoint p : model.points) {
+            int c = colors[p.index];
+            int r = (c & LXColor.RED_MASK) >>> LXColor.RED_SHIFT;
+            int g = (c & LXColor.GREEN_MASK) >>> LXColor.GREEN_SHIFT;
+            int b = c & LXColor.BLUE_MASK;
+
+            rgb2lab(r, g, b, lab);
+
+            lab[0] += L.getValuef();
+            lab[1] += A.getValuef();
+            lab[2] += B.getValuef();
+
+            lab[0] = Math.max(Math.min(lab[0], 100), -100);
+
+            colors[p.index] = lab2rgb(lab);
+        }
+    }
+
+    // adapted from https://github.com/antimatter15/rgb-lab
+    private static int lab2rgb(float[] lab) {
+        float y = (lab[0] + 16) / 116f;
+        float x = lab[1] / 500f + y;
+        float z = y - lab[2] / 200f;
+        float r, g, b;
+
+        x = 0.95047f * ((x * x * x > 0.008856f) ? x * x * x : (x - 16f / 116f) / 7.787f);
+        y = 1.00000f * ((y * y * y > 0.008856f) ? y * y * y : (y - 16f / 116f) / 7.787f);
+        z = 1.08883f * ((z * z * z > 0.008856f) ? z * z * z : (z - 16f / 116f) / 7.787f);
+
+        r = x *    3.2406f + y * -1.5372f + z * -0.4986f;
+        g = x * -0.9689f + y *    1.8758f + z *    0.0415f;
+        b = x *    0.0557f + y * -0.2040f + z *    1.0570f;
+
+        r = (r > 0.0031308f) ? (1.055f * (float)FastMath.pow(r, 1 / 2.4f) - 0.055f) : 12.92f * r;
+        g = (g > 0.0031308f) ? (1.055f * (float)FastMath.pow(g, 1 / 2.4f) - 0.055f) : 12.92f * g;
+        b = (b > 0.0031308f) ? (1.055f * (float)FastMath.pow(b, 1 / 2.4f) - 0.055f) : 12.92f * b;
+
+        return LXColor.rgb(
+                (int)(Math.max(0, Math.min(1, r)) * 255),
+                (int)(Math.max(0, Math.min(1, g)) * 255),
+                (int)(Math.max(0, Math.min(1, b)) * 255)
+        );
+    }
+
+    private static void rgb2lab(int R, int G, int B, float[] lab) {
+        float r = R / 255f;
+        float g = G / 255f;
+        float b = B / 255f;
+        float x, y, z;
+
+        r = (r > 0.04045f) ? (float)FastMath.pow((r + 0.055f) / 1.055f, 2.4f) : r / 12.92f;
+        g = (g > 0.04045f) ? (float)FastMath.pow((g + 0.055f) / 1.055f, 2.4f) : g / 12.92f;
+        b = (b > 0.04045f) ? (float)FastMath.pow((b + 0.055f) / 1.055f, 2.4f) : b / 12.92f;
+
+        x = (r * 0.4124f + g * 0.3576f + b * 0.1805f) / 0.95047f;
+        y = (r * 0.2126f + g * 0.7152f + b * 0.0722f) / 1.00000f;
+        z = (r * 0.0193f + g * 0.1192f + b * 0.9505f) / 1.08883f;
+
+        x = (x > 0.008856f) ? (float)FastMath.pow(x, 1f / 3f) : (7.787f * x) + 16f / 116f;
+        y = (y > 0.008856f) ? (float)FastMath.pow(y, 1f / 3f) : (7.787f * y) + 16f / 116f;
+        z = (z > 0.008856f) ? (float)FastMath.pow(z, 1f / 3f) : (7.787f * z) + 16f / 116f;
+
+        lab[0] = (116 * y) - 16;
+        lab[1] = 500 * (x - y);
+        lab[2] = 200 * (y - z);
+    }
+}
