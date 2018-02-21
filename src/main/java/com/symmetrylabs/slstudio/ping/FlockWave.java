2fdee55924c40a54668a261a1b445ae0732326b7
Ka-Ping Yee <zestyping@gmail.com>
2018-02-21 18:44:22 -0500

    Allow finer-grained control of ripple (reduce range of the dial to -5 to 5).

--- src/main/java/com/symmetrylabs/slstudio/ping/FlockWave.java
+++ src/main/java/com/symmetrylabs/slstudio/ping/FlockWave.java
@@ -51,7 +51,7 @@ public class FlockWave extends SLPatternWithMarkers {
 
     CompoundParameter size = new CompoundParameter("size", 100, 0, 2000);  // render radius of each bird (in)
     CompoundParameter detail = new CompoundParameter("detail", 4, 0, 10);  // ripple spatial frequency (number of waves)
-    CompoundParameter ripple = new CompoundParameter("ripple", 0, -10, 10);  // ripple movement (waves/s)
+    CompoundParameter ripple = new CompoundParameter("ripple", 0, -5, 5);  // ripple movement (waves/s)
     DiscreteParameter palette = new DiscreteParameter("palette", ((SLStudioLX) lx).paletteLibrary.getNames());
         // selected colour palette
     CompoundParameter palStart = new CompoundParameter("palStart", 0, 0, 1);  // palette start point (fraction 0 - 1)
