65581d3e8bff0d3daac9a6d4aa3a1fecb04bd6fd
Nathan Argetsinger <narget@umich.edu>
2018-02-24 19:55:39 -0800

    selected strips nissanmodel

--- src/main/java/com/symmetrylabs/slstudio/pattern/SelectedStrip.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/SelectedStrip.java
@@ -1,41 +1,60 @@
 package com.symmetrylabs.slstudio.pattern;
 
+import com.symmetrylabs.slstudio.model.*;
+import com.symmetrylabs.slstudio.pixlites.NissanPixlite;
 import heronarts.lx.LX;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
 
 import com.symmetrylabs.slstudio.SLStudio;
-import com.symmetrylabs.slstudio.model.Slice;
-import com.symmetrylabs.slstudio.model.Strip;
-import com.symmetrylabs.slstudio.model.Sun;
 import com.symmetrylabs.slstudio.pattern.SunsPattern;
 
-public class SelectedStrip extends SunsPattern {
+//public class SelectedStrip extends SunsPattern {
+//
+//   public SelectedStrip(LX lx) {
+//     super(lx);
+//     addParameter(SLStudio.applet.selectedStrip);
+//   }
+//
+//   public void run(double deltaMs) {
+//     setColors(0);
+//
+//     for (Sun sun : model.getSuns()) {
+//       for (Slice slice : sun.getSlices()) {
+//         int stripIndex = SLStudio.applet.selectedStrip.getValuei();
+//
+//         if (stripIndex > slice.getStrips().size()) {
+//           break;
+//         }
+//
+//         Strip strip = slice.getStrips().get(SLStudio.applet.selectedStrip.getValuei() - 1);
+//
+//         for (LXPoint p : strip.points) {
+//           colors[p.index] = LXColor.RED;
+//         }
+//       }
+//     }
+//   }
+// }
 
-     public SelectedStrip(LX lx) {
-         super(lx);
-         addParameter(SLStudio.applet.selectedStrip);
-     }
 
-     public void run(double deltaMs) {
-         setColors(0);
+public class SelectedStrip extends NissanPattern {
 
-         for (Sun sun : model.getSuns()) {
-             for (Slice slice : sun.getSlices()) {
-                 int stripIndex = SLStudio.applet.selectedStrip.getValuei();
+    public SelectedStrip(LX lx) {
+        super(lx);
+        addParameter(SLStudio.applet.selectedStrip);
+    }
 
-                 if (stripIndex > slice.getStrips().size()) {
-                     break;
-                 }
+    public void run(double deltaMs) {
+        setColors(0);
+        for (NissanCar car : this.model.getCars()) {
+            int stripIndex = SLStudio.applet.selectedStrip.getValuei();
+            Strip strip = car.getStrips().get(stripIndex);
 
-                 Strip strip = slice.getStrips().get(SLStudio.applet.selectedStrip.getValuei() - 1);
-
-                 for (LXPoint p : strip.points) {
-                     colors[p.index] = LXColor.RED;
-                 }
-             }
-         }
-     }
- }
-
+            for (LXPoint p : strip.points) {
+                colors[p.index] = LXColor.RED;
+            }
+        }
+    }
+}
 
