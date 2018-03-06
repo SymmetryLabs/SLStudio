c1e15ad3adf470710b93f8e217d7eb42fa6fc043
Nathan Argetsinger <narget@umich.edu>
2018-03-05 20:13:41 -0800

    forgot fixtureviewer

--- src/main/java/com/symmetrylabs/util/DebugPointCloud/FixtureViewer.java
+++ src/main/java/com/symmetrylabs/util/DebugPointCloud/FixtureViewer.java
@@ -1,13 +1,22 @@
 package com.symmetrylabs.util.DebugPointCloud;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import heronarts.lx.model.LXFixture;
 import heronarts.lx.model.LXPoint;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PointCloudViewer {
+public class FixtureViewer {
+
+    Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .excludeFieldsWithoutExposeAnnotation()
+        .create();
+
     class SimplePoint {
         @Expose
         public double[] point = new double[3];
@@ -20,9 +29,29 @@ public class PointCloudViewer {
 
     private List<SimplePoint> pointcloud = new ArrayList<SimplePoint>();
 
-    public PointCloudViewer(LXFixture cloud){
+    public FixtureViewer(LXFixture cloud){
         for (LXPoint pt : cloud.getPoints()){
             pointcloud.add(new SimplePoint(pt.x, pt.y, pt.z));
         }
+    }
+
+    public void viewFixture() throws IOException {
+        SimplePoint[] staticArray =  new SimplePoint[pointcloud.size()];
+        staticArray = pointcloud.toArray(staticArray);
+
+        String text = gson.toJson(staticArray);
+        System.out.println("WRITING TO FILE:");
+        System.out.println(text);
+        try (PrintStream out = new PrintStream(new FileOutputStream("dynamic.json"))) {
+            out.print(text);
+        }
+        Runtime rt = Runtime.getRuntime();
+        //Process pr = rt.exec("cmd /c dir");
+
+        String basePath = new File("").getAbsolutePath();
+        System.out.println(basePath);
+        String command = basePath+"/submodules/openpixelcontrol/bin/gl_server -l " + basePath + "/dynamic.json";
+        System.out.println(command);
+        Process pr = rt.exec(command);
     }
 }
