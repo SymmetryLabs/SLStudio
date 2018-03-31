c7ad7cbd04b0393bfb8543360f1a7011075530f2
Jake <jlampack@gmail.com>
2018-03-30 17:07:22 -0700

    Add a strip lookup table and getStripById method to StripsModel

--- src/main/java/com/symmetrylabs/slstudio/model/StripsModel.java
+++ src/main/java/com/symmetrylabs/slstudio/model/StripsModel.java
@@ -1,8 +1,10 @@
 package com.symmetrylabs.slstudio.model;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import heronarts.lx.model.LXFixture;
 
@@ -12,6 +14,7 @@ import heronarts.lx.model.LXFixture;
 public class StripsModel<T extends Strip> extends SLModel {
     protected final List<T> strips = new ArrayList<>();
     protected final List<T> stripsUnmodifiable = Collections.unmodifiableList(strips);
+    protected final Map<String, Strip> stripTable = new HashMap<>();
 
     public StripsModel() {
         super();
@@ -27,5 +30,9 @@ public class StripsModel<T extends Strip> extends SLModel {
 
     public List<T> getStrips() {
         return stripsUnmodifiable;
+    }
+
+    public Strip getStripById(String id) {
+      return this.stripTable.get(id);
     }
 }
