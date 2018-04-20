37fef407220181cff0bd928fab49cc4b05d4f297
Aaron <aaronopp@gmail.com>
2018-04-19 17:29:51 -0700

    bug with dmg creation, did zip instead

--- create_zip.sh
+++ create_zip.sh
@@ -11,16 +11,16 @@ cd ..
 
 ./gradlew createDmg
 open build/distributions/SLStudio-1.0.dmg
-mkdir dmg_files
+mkdir SLS_zip
 sleep 5
-cp -r /Volumes/SLStudio-1.0/SLStudio.app dmg_files/
-cp *.lxp dmg_files/
+cp -r /Volumes/SLStudio-1.0/SLStudio.app SLS_zip/
+cp *.lxp SLS_zip/
 echo "copying hidden layout file... check to make sure it is populated with the right layout!"
 echo "layout:"
 tail .layout
-cp .layout dmg_files/
+cp .layout SLS_zip/
 
 cd create-dmg
 echo "removing all existing dmg files in create-dmg folder"
 rm *.dmg
-./create-dmg SLStudio$MM-$DD.dmg ../dmg_files/
+./create-dmg SLStudio$MM-$DD.dmg ../SLS_zip/
