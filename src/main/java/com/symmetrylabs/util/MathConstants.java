ee0146be11c22b86b1daa4d1e1973d2f499daac8
Jake <jlampack@gmail.com>
2018-05-22 15:33:28 -0700

    Add TWO_PI to MathConstants

--- src/main/java/com/symmetrylabs/util/MathConstants.java
+++ src/main/java/com/symmetrylabs/util/MathConstants.java
@@ -7,6 +7,7 @@ public class MathConstants {
         public static final float PI = (float) FastMath.PI;
 
         public static final float HALF_PI = PI / 2f;
+        public static final float TWO_PI = PI * 2f;
 
         public static final float DEG_TO_RAD = PI / 180f;
         public static final float RAD_TO_DEG = 180f / PI;
