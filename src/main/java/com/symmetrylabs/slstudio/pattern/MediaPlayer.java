e4b7630bedb2a0248f5cc395142eca4047acd76d
haldean <me@haldean.org>
2018-08-15 16:19:12 -0700

    try again with vlc4j

--- src/main/java/com/symmetrylabs/slstudio/pattern/MediaPlayer.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/MediaPlayer.java
@@ -1,88 +1,91 @@
 package com.symmetrylabs.slstudio.pattern;
 
+import com.sun.jna.Memory;
 import com.symmetrylabs.slstudio.SLStudio;
 import com.symmetrylabs.slstudio.model.SLModel;
 import com.symmetrylabs.slstudio.pattern.base.SLPattern;
 import heronarts.lx.LX;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.transform.LXVector;
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.common.SeekableDemuxerTrack;
-import org.jcodec.common.io.NIOUtils;
-import org.jcodec.common.model.ColorSpace;
-import org.jcodec.common.model.Picture;
-import org.jcodec.scale.ColorUtil;
-import org.jcodec.scale.Transform;
+import uk.co.caprica.vlcj.component.DirectMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.direct.BufferFormat;
+import uk.co.caprica.vlcj.player.direct.DirectMediaPlayer;
+import uk.co.caprica.vlcj.player.direct.RenderCallback;
+import uk.co.caprica.vlcj.player.direct.format.RV32BufferFormat;
 
 import java.io.File;
-import java.io.IOException;
+import java.nio.IntBuffer;
 
 public class MediaPlayer extends SLPattern<SLModel> {
-    private FrameGrab video;
-    private SeekableDemuxerTrack track;
+    private boolean enabled = false;
     private double time = 0;
     private double videoLengthMs = 0;
+
+    /* simple double-buffer for thread-safe frame update */
+    private int[] front = null;
+    private int[] back = null;
+    private int width;
+
+    private DirectMediaPlayerComponent mediaPlayerComponent;
 
     public MediaPlayer(LX lx) {
         super(lx);
 
         File file = new File("C:/Users/willh/code/Symmetry/cube-arig-usc-v1.mp4");
-        try {
-            video = FrameGrab.createFrameGrab(NIOUtils.readableChannel(file));
-            System.out.println("loaded video");
-        } catch (JCodecException | IOException e) {
-            e.printStackTrace();
-            video = null;
+
+        if (!new NativeDiscovery().discover()) {
+            SLStudio.setWarning("MediaPlayer", "VLC not installed or not found");
+            return;
         }
+        enabled = true;
 
-        if (video != null) {
-            track = video.getVideoTrack();
-            videoLengthMs = track.getMeta().getTotalDuration() * 1000;
+        mediaPlayerComponent = new DirectMediaPlayerComponent((w, h) -> {
+            System.out.println(String.format("DMPC w=%d h=%d", w, h));
+            return new RV32BufferFormat(w, h);
+        }) {
+            @Override
+            public void display(DirectMediaPlayer mediaPlayer, Memory[] nativeBuffers, BufferFormat bufferFormat) {
+                Memory byteBuf = nativeBuffers[0];
+                IntBuffer intBuf = byteBuf.getByteBuffer(0, byteBuf.size()).asIntBuffer();
 
-            /*
-            try {
-                video.seekToSecondSloppy(30.0);
-                Picture nativeFrame = video.getNativeFrame();
-                Transform xform = ColorUtil.getTransform(nativeFrame.getColor(), ColorSpace.RGB);
-                picture = Picture.create(nativeFrame.getWidth(), nativeFrame.getHeight(), ColorSpace.RGB);
-                xform.transform(nativeFrame, picture);
-            } catch (IOException | JCodecException e) {
-                e.printStackTrace();
+                if (back == null || back.length != intBuf.limit()) {
+                    System.out.println("MediaPlayer: realloc image buffer");
+                    back = new int[intBuf.limit()];
+                    front = back;
+                }
+                intBuf.get(back);
+                width = bufferFormat.getWidth();
             }
-            */
-        }
+        };
     }
 
     @Override
     public void run(double elapsedMs) {
+        if (!enabled) return;
+
+        if (!mediaPlayerComponent.getMediaPlayer().isPlaying()) {
+            mediaPlayerComponent.getMediaPlayer().playMedia("../cube-arig-usc-v1.mp4");
+            mediaPlayerComponent.getMediaPlayer().skip(30000);
+        }
+
         time += elapsedMs;
         while (time > videoLengthMs) {
             time -= videoLengthMs;
         }
 
-        Picture nativeFrame;
-        try {
-            video.getVideoTrack().nextFrame();
-            nativeFrame = video.getNativeFrame();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Transform xform = ColorUtil.getTransform(nativeFrame.getColor(), ColorSpace.RGB);
-        Picture picture = Picture.create(nativeFrame.getWidth(), nativeFrame.getHeight(), ColorSpace.RGB);
-        xform.transform(nativeFrame, picture);
-        byte[] data = picture.getPlaneData(0);
+        if (front == null) return;
 
         for (LXVector v : getVectors()) {
             int i = (int) v.y;
             int j = (int) v.x;
-            int dataIdx = 3 * (picture.getWidth() * i + j);
+            int dataIdx = 3 * (width * i + j);
+            int vcolor = front[dataIdx];
+
             colors[v.index] = LXColor.rgb(
-                data[dataIdx + 0] + 128,
-                data[dataIdx + 1] + 128,
-                data[dataIdx + 2] + 128);
+                vcolor & 0xFF,
+                (vcolor >> 8) & 0xFF,
+                (vcolor >> 16) & 0xFF);
         }
     }
 }
