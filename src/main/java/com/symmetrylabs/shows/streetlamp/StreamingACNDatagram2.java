90d95c414d688b8d94e59f80ac4b0f55c3258694
Hike Danakian <hike@hdanak.com>
2018-08-22 01:55:05 -0700

    Fix street lamp output.

---
+++ src/main/java/com/symmetrylabs/shows/streetlamp/StreamingACNDatagram2.java
@@ -0,0 +1,254 @@
+/**
+ * Copyright 2013- Mark C. Slee, Heron Arts LLC
+ *
+ * This file is part of the LX Studio software library. By using
+ * LX, you agree to the terms of the LX Studio Software License
+ * and Distribution Agreement, available at: http://lx.studio/license
+ *
+ * Please note that the LX license is not open-source. The license
+ * allows for free, non-commercial use.
+ *
+ * HERON ARTS MAKES NO WARRANTY, EXPRESS, IMPLIED, STATUTORY, OR
+ * OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF
+ * MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR
+ * PURPOSE, WITH RESPECT TO THE SOFTWARE.
+ *
+ * @author Mark C. Slee <mark@heronarts.com>
+ */
+
+package heronarts.lx.output;
+
+import java.util.List;
+
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.model.LXFixture;
+
+/**
+ * Streaming ACN, also referred to as E1.31, is a standardized protocol for
+ * streaming DMX data over ACN protocol. It's a fairly simple UDP-based wrapper
+ * on 512 bytes of data with a 16-bit universe number.
+ *
+ * See: http://tsp.plasa.org/tsp/documents/docs/E1-31_2009.pdf
+ */
+public class StreamingACNDatagram2 extends LXDatagram {
+
+    protected final static int DMX_DATA_POSITION = 126;
+
+    protected final static int SEQUENCE_NUMBER_POSITION = 111;
+
+    protected final static int UNIVERSE_NUMBER_POSITION = 113;
+
+    private final static int DEFAULT_PORT = 5568;
+
+    private final static int DEFAULT_UNIVERSE_NUMBER = 1;
+
+    private final int[] pointIndices;
+
+    /**
+     * The universe number that this packet sends to.
+     */
+    private int universeNumber;
+
+    public StreamingACNDatagram2(LXFixture fixture) {
+        this(DEFAULT_UNIVERSE_NUMBER, fixture);
+    }
+
+    /**
+     * Constructs a datagram on universe 1
+     *
+     * @param pointIndices Points to send on this universe
+     */
+    public StreamingACNDatagram2(int[] pointIndices) {
+        this(DEFAULT_UNIVERSE_NUMBER, pointIndices);
+    }
+
+    public StreamingACNDatagram2(int universeNumber, LXFixture fixture) {
+        this(universeNumber, getIndices(fixture));
+    }
+
+    private static int[] getIndices(LXFixture fixture) {
+        List<LXPoint> points = fixture.getPoints();
+        int[] indices = new int[points.size()];
+        int i = 0;
+        for (LXPoint p : points) {
+            indices[i++] = p.index;
+        }
+        return indices;
+    }
+
+    /**
+     * Constructs a datagram, sends the list of point indices on the given
+     * universe number.
+     *
+     * @param universeNumber Universe
+     * @param pointIndices List of point indices to encode in packet
+     */
+    public StreamingACNDatagram2(int universeNumber, int[] pointIndices) {
+        this(universeNumber, pointIndices.length * 3, pointIndices);
+    }
+
+    /**
+     * Subclasses may override for a custom payload with fixed size, not necessarily
+     * based upon an array of point indices - such as custom DMX data
+     *
+     * @param universeNumber Universe number
+     * @param dataSize Data payload size
+     */
+    protected StreamingACNDatagram2(int universeNumber, int dataSize) {
+        this(universeNumber, dataSize, null);
+    }
+
+    private StreamingACNDatagram2(int universeNumber, int dataSize, int[] pointIndices) {
+        super(DMX_DATA_POSITION + dataSize);
+        setPort(DEFAULT_PORT);
+        setUniverseNumber(universeNumber);
+        this.pointIndices = pointIndices;
+
+        int flagLength;
+
+        // Preamble size
+        this.buffer[0] = (byte) 0x00;
+        this.buffer[1] = (byte) 0x10;
+
+        // Post-amble size
+        this.buffer[0] = (byte) 0x00;
+        this.buffer[1] = (byte) 0x10;
+
+        // ACN Packet Identifier
+        this.buffer[4] = (byte) 0x41;
+        this.buffer[5] = (byte) 0x53;
+        this.buffer[6] = (byte) 0x43;
+        this.buffer[7] = (byte) 0x2d;
+        this.buffer[8] = (byte) 0x45;
+        this.buffer[9] = (byte) 0x31;
+        this.buffer[10] = (byte) 0x2e;
+        this.buffer[11] = (byte) 0x31;
+        this.buffer[12] = (byte) 0x37;
+        this.buffer[13] = (byte) 0x00;
+        this.buffer[14] = (byte) 0x00;
+        this.buffer[15] = (byte) 0x00;
+
+        // Flags and length
+        flagLength = 0x00007000 | ((this.buffer.length - 16) & 0x0fffffff);
+        this.buffer[16] = (byte) ((flagLength >> 8) & 0xff);
+        this.buffer[17] = (byte) (flagLength & 0xff);
+
+        // RLP 1.31 Protocol PDU Identifier
+        this.buffer[18] = (byte) 0x00;
+        this.buffer[19] = (byte) 0x00;
+        this.buffer[20] = (byte) 0x00;
+        this.buffer[21] = (byte) 0x04;
+
+        // Sender's CID
+        for (int i = 22; i < 38; ++i) {
+            this.buffer[i] = (byte) i;
+        }
+
+        // Flags and length
+        flagLength = 0x00007000 | ((this.buffer.length - 38) & 0x0fffffff);
+        this.buffer[38] = (byte) ((flagLength >> 8) & 0xff);
+        this.buffer[39] = (byte) (flagLength & 0xff);
+
+        // DMP Protocol PDU Identifier
+        this.buffer[40] = (byte) 0x00;
+        this.buffer[41] = (byte) 0x00;
+        this.buffer[42] = (byte) 0x00;
+        this.buffer[43] = (byte) 0x02;
+
+        // Source name
+        this.buffer[44] = 'L';
+        this.buffer[45] = 'X';
+        for (int i = 46; i < 108; ++i) {
+            this.buffer[i] = 0;
+        }
+
+        // Priority
+        this.buffer[108] = 100;
+
+        // Reserved
+        this.buffer[109] = 0x00;
+        this.buffer[110] = 0x00;
+
+        // Sequence Number
+        this.buffer[111] = 0x00;
+
+        // Options
+        this.buffer[112] = 0x00;
+
+        // Universe number
+        // 113-114 are done in setUniverseNumber()
+
+        // Flags and length
+        flagLength = 0x00007000 | ((this.buffer.length - 115) & 0x0fffffff);
+        this.buffer[115] = (byte) ((flagLength >> 8) & 0xff);
+        this.buffer[116] = (byte) (flagLength & 0xff);
+
+        // DMP Set Property Message PDU
+        this.buffer[117] = (byte) 0x02;
+
+        // Address Type & Data Type
+        this.buffer[118] = (byte) 0xa1;
+
+        // First Property Address
+        this.buffer[119] = 0x00;
+        this.buffer[120] = 0x00;
+
+        // Address Increment
+        this.buffer[121] = 0x00;
+        this.buffer[122] = 0x01;
+
+        // Property value count
+        int numProperties = 1 + dataSize;
+        this.buffer[123] = (byte) ((numProperties >> 8) & 0xff);
+        this.buffer[124] = (byte) (numProperties & 0xff);
+
+        // DMX Start
+        this.buffer[125] = 0x00;
+    }
+
+    /**
+     * Sets the universe for this datagram
+     *
+     * @param universeNumber DMX universe
+     * @return this
+     */
+    public StreamingACNDatagram2 setUniverseNumber(int universeNumber) {
+        this.universeNumber = (universeNumber &= 0x0000ffff);
+        this.buffer[UNIVERSE_NUMBER_POSITION] = (byte) ((universeNumber >> 8) & 0xff);
+        this.buffer[UNIVERSE_NUMBER_POSITION + 1] = (byte) (universeNumber & 0xff);
+        return this;
+    }
+
+    /**
+     * Universe number for datagram.
+     *
+     * @return Universe number
+     */
+    public int getUniverseNumber() {
+        return this.universeNumber;
+    }
+
+    public void setDmxData(byte data, int channel) {
+        if (channel < 0 || channel >= this.buffer.length - DMX_DATA_POSITION) {
+            throw new IndexOutOfBoundsException("Channel is greater than DMX data length");
+        }
+        this.buffer[DMX_DATA_POSITION + channel] = data;
+    }
+
+    public void setDmxData(byte[] data, int channel) {
+        if (channel < 0 || channel > this.buffer.length - DMX_DATA_POSITION - data.length) {
+            throw new IndexOutOfBoundsException("Channel is greater than DMX data length");
+        }
+        System.arraycopy(data, 0, this.buffer, DMX_DATA_POSITION, data.length);
+    }
+
+    protected void advanceFrame() {
+        this.buffer[SEQUENCE_NUMBER_POSITION]++;
+    }
+
+    @Override
+    public void onSend(int[] colors) {
+        advanceFrame();
+        copyPoints(colors, this.pointIndices, DMX_DATA_POSITION);
+    }
+}
--- src/main/java/com/symmetrylabs/shows/streetlamp/StreetLampLayout.java
+++ src/main/java/com/symmetrylabs/shows/streetlamp/StreetLampLayout.java
@@ -1,44 +1,60 @@
-package com.symmetrylabs.layouts.streetlamp;
+package com.symmetrylabs.shows.streetlamp;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXFixture;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.output.LXDatagramOutput;
-import heronarts.lx.output.StreamingACNDatagram;
+import heronarts.lx.output.StreamingACNDatagram2;
 
-import com.symmetrylabs.layouts.Layout;
+import com.symmetrylabs.shows.Show;
 import com.symmetrylabs.slstudio.SLStudioLX;
+import com.symmetrylabs.slstudio.model.SLModel;
 import com.symmetrylabs.slstudio.model.StripsModel;
 import com.symmetrylabs.slstudio.output.SimplePixlite;
 import com.symmetrylabs.slstudio.output.PointsGrouping;
 
-public class StreetLampLayout implements Layout {
+public class StreetLampLayout implements Show {
 
     public SLModel buildModel() {
         List<LXPoint> points = new ArrayList<>();
 
-        points.add(new LXPoint(0, 0, 0));
-        points.add(new LXPoint(0, 0, 0));
-        points.add(new LXPoint(0, 0, 0));
+        points.add(new LXPoint(0, 0, 100));
+        points.add(new LXPoint(-100, 0, 0));
+        points.add(new LXPoint(100, 0, 0));
 
         return new SLModel(points);
     }
 
     public void setupLx(SLStudioLX lx) {
-        LXDatagramOutput output = new LXDatagramOutput(lx);
+        try {
+            LXDatagramOutput output = new LXDatagramOutput(lx);
 
-        for (LXPoint point : lx.model.getPoints()) {
-            output.addDatagram(new RGBWACNDatagram(lx.model));
+            output.addDatagram(new RGBWACNDatagram(lx.model).setAddress("10.200.1.16"));
+
+            lx.addOutput(output);
         }
-
-        lx.addOutput(output);
+        catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
-    static class RGBWACNDatagram extends StreamingACNDatagram {
+    static class RGBWACNDatagram extends StreamingACNDatagram2 {
+        public RGBWACNDatagram(LXFixture fixture) {
+            super(1, fixture.getPoints().size() * 4);
+        }
+
         protected RGBWACNDatagram copyPoints(int[] colors, int[] pointIndices, int offset) {
             int i = offset;
-            int[] byteOffset = BYTE_ORDERING[this.byteOrder.ordinal()];
 
-            for (int index : pointIndices) {
-                int color = (index >= 0) ? colors[index] : 0;
+            for (int color : colors) {
+                if (i >= buffer.length)
+                    break;
+
                 float saturation = LXColor.s(color) / 100;
                 float brightness = LXColor.b(color) / 100;
 
@@ -47,10 +63,10 @@ public class StreetLampLayout implements Layout {
                 byte blue = (byte)((color & 0xff) * saturation);
                 byte white = (byte)((1 - saturation) * brightness * 255);
 
-                this.buffer[i + byteOffset[0]] = red;
-                this.buffer[i + byteOffset[1]] = green;
-                this.buffer[i + byteOffset[2]] = blue;
-                this.buffer[i + 3] = white;
+                buffer[i] = green;
+                buffer[i + 1] = blue;
+                buffer[i + 2] = white;
+                buffer[i + 3] = red;
 
                 i += 4;
             }
