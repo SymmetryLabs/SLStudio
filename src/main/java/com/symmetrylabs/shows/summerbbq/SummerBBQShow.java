aac634070c966215660e0a762c99e8924dae3e98
eggmode <cameronnagai@gmail.com>
2018-08-21 20:18:49 -0700

    Offset strings in opposite direction.

--- src/main/java/com/symmetrylabs/shows/summerbbq/SummerBBQShow.java
+++ src/main/java/com/symmetrylabs/shows/summerbbq/SummerBBQShow.java
@@ -19,7 +19,7 @@ public class SummerBBQShow implements Show {
     public StripsModel<DoubleStrip> buildModel() {
 
         DoubleStrip.Metrics stripMetrics = new DoubleStrip.Metrics(
-                LED_PER_STRIP, STRIP_LENGTH / LED_PER_STRIP, 1, STRIP_LENGTH / LED_PER_STRIP / 2); // 1" front/back gap
+                LED_PER_STRIP, STRIP_LENGTH / LED_PER_STRIP, 1, -STRIP_LENGTH / LED_PER_STRIP / 2); // 1" front/back gap
 
         CirclesBuilder<DoubleStrip> builder = new CirclesBuilder<>(
                 (String id, LXTransform t) -> new DoubleStrip(id, stripMetrics, t));
@@ -31,19 +31,19 @@ public class SummerBBQShow implements Show {
 
         for (double radius : circleRadii) {
             builder.addCircle().withRadius(radius * METER * scale)
-                .addStrips(15).withDegreeOffset(90).withDegreeSweep(-72)
+                .addStrips(15).withDegreeOffset(90).withDegreeSweep(-360 / 5)
                 .build();
 
             builder.addCircle().withRadius(radius * METER * scale)
-                .addStrips(15).withDegreeOffset(90 - 360 / 5).withDegreeSweep(-72)
+                .addStrips(15).withDegreeOffset(90 - 360 / 5).withDegreeSweep(-360 / 5)
                 .build();
 
             builder.addCircle().withRadius(radius * METER * scale)
-                .addStrips(15).withDegreeOffset(90).withDegreeSweep(72)
+                .addStrips(15).withDegreeOffset(90).withDegreeSweep(360 / 5)
                 .build();
 
             builder.addCircle().withRadius(radius * METER * scale)
-                .addStrips(15).withDegreeOffset(90 + 360 / 5).withDegreeSweep(72)
+                .addStrips(15).withDegreeOffset(90 + 360 / 5).withDegreeSweep(360 / 5)
                 .build();
         }
 
