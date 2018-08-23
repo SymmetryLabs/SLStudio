5b6421dad507b339bee73374b1801057eba5b94b
haldean <me@haldean.org>
2018-08-23 12:40:31 -0500

    add morph video player

---
+++ src/main/java/com/symmetrylabs/shows/pilots/MorphPlayer.java
@@ -0,0 +1,29 @@
+package com.symmetrylabs.shows.pilots;
+
+import com.symmetrylabs.slstudio.pattern.VideoPlayer;
+import heronarts.lx.LX;
+
+public class MorphPlayer extends VideoPlayer {
+    public static final String GROUP_NAME = PilotsShow.SHOW_NAME;
+
+    public MorphPlayer(LX lx) {
+        super(lx);
+
+        removeParameter(chooseFileParam);
+        removeParameter(captureParam);
+
+        mediaUrl = "../cube-arig-usc-v1.mp4";
+        mediaOptions = null;
+        cropTop = 20;
+        cropLeft = 16;
+        cropRight = 492;
+        cropBottom = 483;
+
+        playParam.setValue(false);
+    }
+
+    @Override
+    public long getStartTimeMs() {
+        return 54_000L;
+    }
+}
