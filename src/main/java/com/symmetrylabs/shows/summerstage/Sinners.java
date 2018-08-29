c9ca930dd7e03e9f2080c4ab19e00db6464ec5a7
Raphael Palefsky-Smith <rpalefsk@stanford.edu>
2018-08-29 13:50:56 -0400

    Add density to Sinners

--- src/main/java/com/symmetrylabs/shows/summerstage/Sinners.java
+++ src/main/java/com/symmetrylabs/shows/summerstage/Sinners.java
@@ -27,45 +27,73 @@ import com.symmetrylabs.shows.summerstage.SummerStageShow;
 
 public class Sinners extends SLPattern<CubesModel> {
     public static final String GROUP_NAME = SummerStageShow.SHOW_NAME;
-    
+
     ArrayList<Float> offsets = new ArrayList();
     ArrayList<Float> multipliers = new ArrayList();
     ArrayList<Float> bases = new ArrayList();
+    ArrayList<Integer> order = new ArrayList();
+
     float n = 0;
 
-    CompoundParameter hue = new CompoundParameter("hue", 0.07);
-    CompoundParameter hvar = new CompoundParameter("hvar", 0.07);
+    CompoundParameter hue = new CompoundParameter("hue", 0.86);
+    CompoundParameter hvar = new CompoundParameter("hvar", 0.18);
     CompoundParameter sat = new CompoundParameter("sat", 0.9);
-    CompoundParameter speed = new CompoundParameter("speed", 7, 30);
+    CompoundParameter speed = new CompoundParameter("speed", 7, 0, 30);
+    CompoundParameter density = new CompoundParameter("density", 1.0);
+    CompoundParameter densityFade = new CompoundParameter("dfade", 0.1);
+
+    CompoundParameter minPeriod = new CompoundParameter("minPeriod", 0, 0, 1);
+    CompoundParameter maxPeriod = new CompoundParameter("maxPeriod", 1, 0, 1);
+
+
 
     public Sinners(LX lx) {
         super(lx);
 
-        for (int i = 0; i < model.points.length; i++) {
+        for (int i = 0; i < model.getCubes().size(); i++) {
             offsets.add(MathUtils.random(1));
             multipliers.add(MathUtils.random(1));
             bases.add(MathUtils.random(1));
+            order.add(i);
         }
+        Collections.shuffle(order);
 
         addParameter(hvar);
         addParameter(hue);
         addParameter(sat);
         addParameter(speed);
+        addParameter(density);
+        addParameter(densityFade);
+        addParameter(minPeriod);
+        addParameter(maxPeriod);
     }
 
     public void run(double deltaMs) {
         List<CubesModel.Cube> cubes = model.getCubes();
 
+        
         for (int i = 0; i < cubes.size(); i++) {
             CubesModel.Cube cube = cubes.get(i);
+
+            int o = order.get(i);
+            float oR = (float)o / (float)order.size();
+            float falloff = 1.0f;
+            float dens = density.getValuef();
+            float maxDist = densityFade.getValuef();
+            if (oR >= dens) {
+                float dist = MathUtils.min(oR - dens, maxDist);
+                falloff = MathUtils.map(dist, 0, maxDist, 1.0f, 0.0f);
+            }
+
             float pos = (float) i / (float) cubes.size();
-            float s = sin(multipliers.get(i) * n + offsets.get(i));
+            float m = MathUtils.map(multipliers.get(i), 0, 1, minPeriod.getValuef(), maxPeriod.getValuef());
+            float s = sin(m * n + offsets.get(i));
             s = (s + 1.0f) / 2.0f;
             float c = bases.get(i) + NoiseUtils.noise(pos + n / 100.0f) / 2;
             float huevar = hvar.getValuef();
             float actual_c = (hue.getValuef()) + (c * huevar);
 
-            setColor(cube, LXColor.hsb(actual_c * 360, sat.getValuef() * 100, s * 100));
+            setColor(cube, LXColor.hsb(actual_c * 360, sat.getValuef() * 100, s * 100 * falloff));
         }
 
         n += speed.getValuef() / 1000.0 * deltaMs;
