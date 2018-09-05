bca08305438b9ceaf8c107e078a4656b48701b93
haldean <me@haldean.org>
2018-09-05 11:46:16 -0700

    add UI for changing which cart is where

---
+++ src/main/java/com/symmetrylabs/shows/pilots/CartConfigurator.java
@@ -0,0 +1,96 @@
+package com.symmetrylabs.shows.pilots;
+
+import heronarts.lx.parameter.StringParameter;
+import heronarts.p3lx.ui.UI;
+import heronarts.p3lx.ui.component.UIButton;
+import heronarts.p3lx.ui.component.UILabel;
+import heronarts.p3lx.ui.component.UITextBox;
+import heronarts.p3lx.ui.studio.UICollapsibleSection;
+import processing.core.PConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartConfigurator extends UICollapsibleSection {
+    private static final int PAD = 4;
+
+    public interface ConfigChangedListener {
+        void onConfigChanged(CartConfig[] configs);
+    }
+
+    private final List<StringParameter> addrParams;
+    private ConfigChangedListener listener;
+
+    public CartConfigurator(UI ui, float x, float y, float w) {
+        super(ui, x, y, w, 220);
+        setTitle("MODULE ADDRESSES");
+
+        addrParams = new ArrayList<>();
+        for (String id : CartConfig.ids) {
+            StringParameter p = new StringParameter(id);
+            p.setShouldSerialize(false);
+            addrParams.add(p);
+        }
+
+        final float labelWidth = 50;
+        final float inputWidth = getContentWidth() - labelWidth - PAD;
+        final float elemHeight = 20;
+        float yAccum = PAD;
+
+        for (StringParameter addrParam : addrParams) {
+            new UILabel(PAD, yAccum, labelWidth, elemHeight)
+                .setLabel(addrParam.getLabel())
+                .setTextAlignment(PConstants.LEFT, PConstants.CENTER)
+                .addToContainer(this);
+            new UITextBox(PAD + labelWidth, yAccum, inputWidth, elemHeight)
+                .setParameter(addrParam)
+                .addToContainer(this);
+            yAccum += elemHeight + PAD;
+        }
+
+        new SaveButton(getContentWidth() - labelWidth, yAccum, labelWidth, elemHeight).addToContainer(this);
+        yAccum += elemHeight + PAD;
+        System.out.println(yAccum);
+    }
+
+    public void setListener(ConfigChangedListener listener) {
+        this.listener = listener;
+    }
+
+    public void applyConfigs(CartConfig[] configs) {
+        for (CartConfig cc : configs) {
+            for (StringParameter p : addrParams) {
+                if (p.getLabel().equals(cc.modelId)) {
+                    p.setValue(cc.address);
+                }
+            }
+        }
+    }
+
+    private void sendUpdate() {
+        if (listener != null) {
+            CartConfig[] configs = new CartConfig[addrParams.size()];
+            for (int i = 0; i < addrParams.size(); i++) {
+                StringParameter p = addrParams.get(i);
+                configs[i] = new CartConfig(p.getLabel(), p.getString());
+            }
+            listener.onConfigChanged(configs);
+        }
+    }
+
+    private class SaveButton extends UIButton {
+        public SaveButton(float x, float y, float w, float h) {
+            super(x, y, w, h);
+            setLabel("Save");
+            setMomentary(true);
+            setTextAlignment(PConstants.CENTER, PConstants.CENTER);
+        }
+
+        @Override
+        protected void onToggle(boolean active) {
+            if (active) {
+                sendUpdate();
+            }
+        }
+    }
+}
--- src/main/java/com/symmetrylabs/shows/pilots/PilotsShow.java
+++ src/main/java/com/symmetrylabs/shows/pilots/PilotsShow.java
@@ -4,20 +4,19 @@ import com.symmetrylabs.shows.Show;
 import com.symmetrylabs.slstudio.SLStudioLX;
 import com.symmetrylabs.slstudio.model.SLModel;
 import com.symmetrylabs.slstudio.model.Strip;
-import com.symmetrylabs.slstudio.model.StripsModel;
+import com.symmetrylabs.slstudio.ui.UIOverriddenRightPane;
 import heronarts.lx.color.LXColor;
-import heronarts.lx.model.LXPoint;
+import heronarts.lx.output.LXOutput;
 import heronarts.lx.transform.LXVector;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
  * This file creates and positions "carts" and creates one pixlite for each of them
  */
-public class PilotsShow implements Show {
+public class PilotsShow implements Show, CartConfigurator.ConfigChangedListener {
     static final String SHOW_NAME = "pilots";
 
     static final float RED_HUE = 0;
@@ -25,28 +24,32 @@ public class PilotsShow implements Show {
     static final int RED = LXColor.hsb(RED_HUE, 100, 100);
     static final int YELLOW = LXColor.hsb(YELLOW_HUE, 85, 100);
 
+    private SLStudioLX lx;
+    private CartConfigurator configurator;
+    private List<PilotsPixlite> outputs = new ArrayList<>();
+
     private PilotsModel.Cart[] carts = new PilotsModel.Cart[] {
         /* no extra cart spacing here because they're not actually adjacent;
          * FSL is in front of BSL. */
-        new PilotsModel.Cart("FSL",  new LXVector(
+        new PilotsModel.Cart(CartConfig.FSL,  new LXVector(
             24 * PilotsModel.STRIP_LENGTH + 4 * PilotsModel.CART_SPACING, 0, 0)),
 
-        new PilotsModel.Cart("BSL",  new LXVector(
+        new PilotsModel.Cart(CartConfig.BSL,  new LXVector(
             20 * PilotsModel.STRIP_LENGTH + 4 * PilotsModel.CART_SPACING, 0, 3 * PilotsModel.STRIP_LENGTH)),
 
-        new PilotsModel.Cart("BSCL", new LXVector(
+        new PilotsModel.Cart(CartConfig.BSCL, new LXVector(
             16 * PilotsModel.STRIP_LENGTH + 3 * PilotsModel.CART_SPACING, 0, 3 * PilotsModel.STRIP_LENGTH)),
 
-        new PilotsModel.Cart("BSC",  new LXVector(
+        new PilotsModel.Cart(CartConfig.BSC,  new LXVector(
             12 * PilotsModel.STRIP_LENGTH + 2 * PilotsModel.CART_SPACING, 0, 3 * PilotsModel.STRIP_LENGTH)),
 
-        new PilotsModel.Cart("BSCR", new LXVector(
+        new PilotsModel.Cart(CartConfig.BSCR, new LXVector(
             8 * PilotsModel.STRIP_LENGTH + PilotsModel.CART_SPACING, 0, 3 * PilotsModel.STRIP_LENGTH)),
 
-        new PilotsModel.Cart("BSR",  new LXVector(
+        new PilotsModel.Cart(CartConfig.BSR,  new LXVector(
             4 * PilotsModel.STRIP_LENGTH, 0, 3 * PilotsModel.STRIP_LENGTH)),
 
-        new PilotsModel.Cart("FSR",  new LXVector(
+        new PilotsModel.Cart(CartConfig.FSR,  new LXVector(
             0, 0, 0)),
     };
 
@@ -61,16 +64,30 @@ public class PilotsShow implements Show {
 
     @Override
     public void setupLx(SLStudioLX lx) {
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.10", ((PilotsModel)lx.model).getCartById("FSL")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.11", ((PilotsModel)lx.model).getCartById("BSL")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.12", ((PilotsModel)lx.model).getCartById("BSCL")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.13", ((PilotsModel)lx.model).getCartById("BSC")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.14", ((PilotsModel)lx.model).getCartById("BSCR")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.15", ((PilotsModel)lx.model).getCartById("BSR")));
-        lx.addOutput(new PilotsPixlite(lx, "10.200.1.16", ((PilotsModel)lx.model).getCartById("FSR")));
+        this.lx = lx;
+        onConfigChanged(CartConfig.defaultConfigs());
     }
 
     @Override
     public void setupUi(SLStudioLX lx, SLStudioLX.UI ui) {
+        configurator = new CartConfigurator(ui, 0, 0, ui.rightPane.utility.getContentWidth());
+        configurator.setListener(this);
+        configurator.applyConfigs(CartConfig.defaultConfigs());
+        configurator.addToContainer(ui.rightPane.utility);
+    }
+
+    @Override
+    public void onConfigChanged(CartConfig[] configs) {
+        for (LXOutput output : outputs) {
+            lx.removeOutput(output);
+        }
+        PilotsModel model = (PilotsModel) lx.model;
+        outputs.clear();
+        for (CartConfig cc : configs) {
+            outputs.add(new PilotsPixlite(lx, cc.address, model.getCartById(cc.modelId)));
+        }
+        for (LXOutput output : outputs) {
+            lx.addOutput(output);
+        }
     }
 }
