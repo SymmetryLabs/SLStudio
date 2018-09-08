be721e5f84e40c22738e8ab0562dd51253318ebf
Ka-Ping Yee <zestyping@gmail.com>
2018-09-08 10:58:37 -0400

    Add SoftEdge parameter to RedRise.

--- src/main/java/com/symmetrylabs/shows/summerstage/RedRise.java
+++ src/main/java/com/symmetrylabs/shows/summerstage/RedRise.java
@@ -19,10 +19,11 @@ import heronarts.lx.parameter.LXParameter;
 public class RedRise extends SLPattern<StripsModel<? extends Strip>> {
     public static final String GROUP_NAME = SummerStageShow.SHOW_NAME;
 
-    private CompoundParameter hueParam = new CompoundParameter("Hue", 0, -1, 1).setDescription("Hue adjustment");
-    private CompoundParameter riseSpdParam = new CompoundParameter("RiseSpd", 10, 0, 100).setDescription("Infection growth speed (strip lengths per minute)");
-    private CompoundParameter splitSpdParam = new CompoundParameter("SplitSpd", 50, 0, 100).setDescription("Infection growth speed (strip lengths per minute)");
+    private CompoundParameter hueParam = new CompoundParameter("Hue", 0, -1, 1);
+    private CompoundParameter riseSpdParam = new CompoundParameter("RiseSpd", 10, 0, 100);
+    private CompoundParameter splitSpdParam = new CompoundParameter("SplitSpd", 50, 0, 100);
     private BooleanParameter triggerParam = new BooleanParameter("Trigger", false).setMode(BooleanParameter.Mode.MOMENTARY);
+    private CompoundParameter softEdgeParam = new CompoundParameter("SoftEdge", 50, 0, 200);
 
     private double timeSec = 0;
     private double riseStartSec = Double.MAX_VALUE;
@@ -35,6 +36,7 @@ public class RedRise extends SLPattern<StripsModel<? extends Strip>> {
         addParameter(riseSpdParam);
         addParameter(splitSpdParam);
         addParameter(triggerParam);
+        addParameter(softEdgeParam);
 
         reset();
     }
@@ -73,28 +75,40 @@ public class RedRise extends SLPattern<StripsModel<? extends Strip>> {
     public void run(double deltaMs, PolyBuffer.Space preferredSpace) {
         double deltaSec = deltaMs/1000.0;
         timeSec += deltaSec;
+        float softEdge = softEdgeParam.getValuef();
 
-        double duration;
-        double elapsedSec;
+        float duration;
+        float elapsedSec;
         int[] colors = (int[]) getArray(Space.RGB8);
 
         if (timeSec > splitStartSec) {
-            elapsedSec = timeSec - splitStartSec;
-            duration = (60.0 / splitSpdParam.getValue());
+            elapsedSec = (float) (timeSec - splitStartSec);
+            duration = (60f / splitSpdParam.getValuef());
             if (elapsedSec > duration) {
                 reset();
             } else {
-                double radiusX = (model.xRange / 2) * elapsedSec / duration;
+                float radiusX = (model.xRange / 2) * elapsedSec / duration;
                 for (LXPoint p : model.points) {
-                    if (Math.abs(p.x - model.cx) < radiusX) colors[p.index] = Ops8.BLACK;
+                    if (Math.abs(p.x - model.cx) < radiusX) {
+                        float b = 1 - (radiusX - Math.abs(p.x - model.cx)) / softEdge;
+                        if (b <= 0) colors[p.index] = Ops8.BLACK;
+                        else {
+                            int c = colors[p.index];
+                            colors[p.index] = Ops8.rgba(Ops8.red(c), Ops8.green(c), Ops8.blue(c), (int) (b * 255 + 0.5));
+                        }
+                    }
                 }
             }
         } else if (timeSec > riseStartSec) {
-            elapsedSec = timeSec - riseStartSec;
-            duration = (60.0 / riseSpdParam.getValue());
-            double topY = model.yMin + (model.yRange * elapsedSec / duration);
+            elapsedSec = (float) (timeSec - riseStartSec);
+            duration = (60f / riseSpdParam.getValuef());
+            float topY = model.yMin + (model.yRange * elapsedSec / duration);
             for (LXPoint p : model.points) {
-                if (p.y < topY) colors[p.index] = LXColor.hsb(hueParam.getValuef(), 100, 100);
+                if (p.y < topY) {
+                    float b = (topY - p.y) / softEdge;
+                    if (b > 1) b = 1;
+                    colors[p.index] = LXColor.hsb(hueParam.getValuef(), 100, b * 100);
+                }
             }
         }
 
