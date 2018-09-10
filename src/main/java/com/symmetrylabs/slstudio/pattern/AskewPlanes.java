0de3d7132e741e29d88757abe8d0ede220fed505
haldean <me@haldean.org>
2018-09-10 11:06:15 -0700

    re-import askew planes from master

--- src/main/java/com/symmetrylabs/slstudio/pattern/AskewPlanes.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/AskewPlanes.java
@@ -15,70 +15,23 @@ public class AskewPlanes extends DPat {
 
     DiscreteParameter numPlanes = new DiscreteParameter("Number", new String[]{"3", "2", "1"});
 
-    CompoundParameter a1Period = new CompoundParameter( "aP1", 1, 0, 100);
-    CompoundParameter b1Period = new CompoundParameter( "bP1", 1, 0, 100);
-    CompoundParameter c1Period = new CompoundParameter( "cP1", 1, 0, 100);
-
-    CompoundParameter a2Period = new CompoundParameter( "aP2", 1, 0, 100);
-    CompoundParameter b2Period = new CompoundParameter( "bP2", 1, 0, 100);
-    CompoundParameter c2Period = new CompoundParameter( "cP2", 1, 0, 100);
-
-    CompoundParameter a3Period = new CompoundParameter( "aP3", 1, 0, 100);
-    CompoundParameter b3Period = new CompoundParameter( "bP3", 1, 0, 100);
-    CompoundParameter c3Period = new CompoundParameter( "cP3", 1, 0, 100);
-
-    CompoundParameter speed1 = new CompoundParameter("speed1", 1, 0, 1000);
-    CompoundParameter speed2= new CompoundParameter("speed2", 1, 0, 1000);
-    CompoundParameter speed3 = new CompoundParameter("speed3", 1, 0, 1000);
     CompoundParameter thickness = new CompoundParameter("Thick", 0.2, 0.1, 0.9);
     class Plane {
-        private SinLFO a = null;
-        private SinLFO b = null;
-        private SinLFO c = null;
+        private final SinLFO a;
+        private final SinLFO b;
+        private final SinLFO c;
         float av = 1;
         float bv = 1;
         float cv = 1;
         float denom = 0.1f;
 
         Plane(int i) {
-            if (i == 0) {
-                CompoundParameter innera1 = AskewPlanes.this.a1Period;
-                CompoundParameter innerb1 = AskewPlanes.this.b1Period;
-                CompoundParameter innerc1 = AskewPlanes.this.c1Period;
-                addModulator(a = new SinLFO(-1, 1, 4000 + innera1.getValuef() * 1029 * i)).trigger();
-                addModulator(b = new SinLFO(-1, 1, 11000 - innerb1.getValuef() * 1104 * i)).trigger();
-
-                addModulator(c = new SinLFO(-50, 50, 4000 + innerc1.getValuef()* 1000 * i * ((i % 2 == 0) ? 1 : -1))).trigger();
-            }
-
-            else if (i == 1)  {
-                CompoundParameter innera2 = AskewPlanes.this.a2Period;
-                CompoundParameter innerb2 = AskewPlanes.this.b2Period;
-                CompoundParameter innerc2 = AskewPlanes.this.c2Period;
-                addModulator(a = new SinLFO(-1, 1, 4000*speed2.getValuef() + innera2.getValuef() * 1029 * i)).trigger();
-                addModulator(b = new SinLFO(-1, 1, 11000*speed2.getValuef() - innerb2.getValuef() * 1104 * i)).trigger();
-
-                addModulator(c = new SinLFO(
-                    -50, 50, 4000 *speed2.getValuef() + innerc2.getValuef() * 1000 * i * ((i % 2 == 0) ? 1 : -1))).trigger();
-                }
-
-            else if (i == 2) {
-            CompoundParameter innera3 = AskewPlanes.this.a3Period;
-            CompoundParameter innerb3 = AskewPlanes.this.b3Period;
-            CompoundParameter innerc3 = AskewPlanes.this.c3Period;
-            addModulator(a = new SinLFO(-1, 1, 4000*speed3.getValuef() + innera3.getValuef() * 1029 * i)).trigger();
-            addModulator(b = new SinLFO(-1, 1, 11000*speed3.getValuef() - innerb3.getValuef() * 1104 * i)).trigger();
-
-            addModulator(c = new SinLFO(
-                -50, 50, 4000 *speed3.getValuef() + innerc3.getValuef() * 1000 * i * ((i % 2 == 0) ? 1 : -1))).trigger();
+            addModulator(a = new SinLFO(-1, 1, 4000 + 1029 * i)).trigger();
+            addModulator(b = new SinLFO(-1, 1, 11000 - 1104 * i)).trigger();
+            addModulator(c = new SinLFO(-50, 50, 4000 + 1000 * i * ((i % 2 == 0) ? 1 : -1))).trigger();
         }
 
-
-        }
-
-
-
-        public void run(double deltaMs) {
+        void run(double deltaMs) {
             av = a.getValuef();
             bv = b.getValuef();
             cv = c.getValuef();
@@ -95,25 +48,9 @@ public class AskewPlanes extends DPat {
         planes = new Plane[NUM_PLANES];
         for (int i = 0; i < planes.length; ++i) {
             planes[i] = new Plane(i);
-
         }
         pTransX.setValue(1);
-//        addParameter(a1Period);
-//        addParameter(b1Period);
-//        addParameter(c1Period);
-//
-//        addParameter(a2Period);
-//        addParameter(b2Period);
-//        addParameter(c2Period);
-//
-//        addParameter(a3Period);
-//        addParameter(b3Period);
-//        addParameter(c3Period);
-//
-//        addParameter(speed1);
-//        addParameter(speed2);
-//        addParameter(speed3);
-//        addParameter(numPlanes);
+        addParameter(numPlanes);
         removeParameter(pRotX);
         removeParameter(pRotY);
         removeParameter(pRotZ);
