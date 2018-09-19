bdeba2fc61f68d3bdb390ecd95cb4d83550d5562
Haldean Brown <me@haldean.org>
2018-09-19 14:40:33 -0700

    fix indentation from my previous commit
    My editor was not properly set up to use tabs instead of spaces

--- src/main/java/com/symmetrylabs/slstudio/dmx/DmxUsbOutput.java
+++ src/main/java/com/symmetrylabs/slstudio/dmx/DmxUsbOutput.java
@@ -10,11 +10,11 @@ import heronarts.lx.output.LXOutput;
 import heronarts.lx.color.LXColor;
 
 public class DmxUsbOutput extends LXOutput {
-        public enum RGBWMode {
-                USE_HSB,
-                NO_WHITE,
-                ADD_WHITE,
-        }
+    public enum RGBWMode {
+        USE_HSB,
+        NO_WHITE,
+        ADD_WHITE,
+    }
 
     private DmxUsbWriter dmxWriter;
 
@@ -24,8 +24,8 @@ public class DmxUsbOutput extends LXOutput {
     public static final int BLUE = 240;
 
     private int[] colorChannels = new int[0];
-        private RGBWMode mode = RGBWMode.ADD_WHITE;
-        private GammaExpander gammaExpander = null;
+    private RGBWMode mode = RGBWMode.ADD_WHITE;
+    private GammaExpander gammaExpander = null;
 
     public DmxUsbOutput(LX lx) {
         super(lx);
@@ -43,53 +43,53 @@ public class DmxUsbOutput extends LXOutput {
         this.colorChannels = colorChannels;
     }
 
-        public DmxUsbOutput setGammaExpander(GammaExpander g) {
-                gammaExpander = g;
-                return this;
-        }
+    public DmxUsbOutput setGammaExpander(GammaExpander g) {
+        gammaExpander = g;
+        return this;
+    }
 
-        public DmxUsbOutput setRGBWMode(RGBWMode mode) {
-                this.mode = mode;
-                return this;
-        }
+    public DmxUsbOutput setRGBWMode(RGBWMode mode) {
+        this.mode = mode;
+        return this;
+    }
 
     @Override
     protected void onSend(int[] colors) {
         for (int i = 0; i < colors.length; ++i) {
             int color = colors[i];
 
-                        if (gammaExpander != null) {
-                                color = gammaExpander.getExpandedColor(color);
-                        }
+            if (gammaExpander != null) {
+                color = gammaExpander.getExpandedColor(color);
+            }
 
-                        int red = Ops8.red(color);
-                        int green = Ops8.green(color);
-                        int blue = Ops8.blue(color);
-                        int white = 0;
+            int red = Ops8.red(color);
+            int green = Ops8.green(color);
+            int blue = Ops8.blue(color);
+            int white = 0;
 
-                        switch (mode) {
-                        case USE_HSB: {
-                                float saturation = LXColor.s(color) / 100f;
-                                float brightness = LXColor.b(color) / 100f;
-                                red *= saturation;
-                                green *= saturation;
-                                blue *= saturation;
-                                white = (int)((1 - saturation) * brightness * 255);
-                                break;
-                        }
+            switch (mode) {
+            case USE_HSB: {
+                float saturation = LXColor.s(color) / 100f;
+                float brightness = LXColor.b(color) / 100f;
+                red *= saturation;
+                green *= saturation;
+                blue *= saturation;
+                white = (int)((1 - saturation) * brightness * 255);
+                break;
+            }
 
-                        case NO_WHITE:
-                                break;
+            case NO_WHITE:
+                break;
 
-                        case ADD_WHITE: {
-                                float saturation = LXColor.s(color) / 100f;
-                                float brightness = LXColor.b(color) / 100f;
-                                white = (int)(255 * Math.pow((1.0 - saturation) * brightness, 2));
-                                break;
-                        }
-                        }
+            case ADD_WHITE: {
+                float saturation = LXColor.s(color) / 100f;
+                float brightness = LXColor.b(color) / 100f;
+                white = (int)(255 * Math.pow((1.0 - saturation) * brightness, 2));
+                break;
+            }
+            }
 
-                        for (int j = 0; j < colorChannels.length; ++j) {
+            for (int j = 0; j < colorChannels.length; ++j) {
                 int colorChannel = colorChannels[j];
                 int value = 0;
 
