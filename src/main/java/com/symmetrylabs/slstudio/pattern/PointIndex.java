593d1934efa5aaeca7fe1f5748e1a51226c82c30
Hike Danakian <hike@hdanak.com>
2018-09-18 23:24:28 -0700

    Fix off-by-one error in PointIndex.

--- src/main/java/com/symmetrylabs/slstudio/pattern/PointIndex.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/PointIndex.java
@@ -2,22 +2,22 @@ package com.symmetrylabs.slstudio.pattern;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXPattern;
-import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.color.LXColor;
 
 
 public class PointIndex extends LXPattern {
 
-    private DiscreteParameter indexParam;
+    private CompoundParameter indexParam;
 
     public PointIndex(LX lx) {
         super(lx);
 
-        addParameter(indexParam = new DiscreteParameter("index", 0, 0, lx.model.size));
+        addParameter(indexParam = new CompoundParameter("index", 0, 0, lx.model.size - 1));
     }
 
     public void run(double deltaMs) {
         setColors(LXColor.BLACK);
-        setColor(indexParam.getValuei(), palette.getColor());
+        setColor((int)indexParam.getValue(), palette.getColor());
     }
 }
