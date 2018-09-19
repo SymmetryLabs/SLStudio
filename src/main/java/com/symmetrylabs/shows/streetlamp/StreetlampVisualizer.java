4be8223d9abd1838fad7bac8e035a39d62626ad7
Haldean Brown <me@haldean.org>
2018-09-18 17:07:30 -0700

    add cap to tower visualization

--- src/main/java/com/symmetrylabs/shows/streetlamp/StreetlampVisualizer.java
+++ src/main/java/com/symmetrylabs/shows/streetlamp/StreetlampVisualizer.java
@@ -9,6 +9,10 @@ import processing.core.PGraphics;
 
 public class StreetlampVisualizer extends UI3dComponent {
     private static final float TOWER_H = 14 * 12;
+    private static final float TOP_PLANE = 5 * StreetlampModel.ELEMENT_Y_DELTA;
+
+    private static final int TOWER_COLOR = 0xFF555555;
+
     private final P3LX lx;
     private final LXModel model;
 
@@ -20,20 +24,19 @@ public class StreetlampVisualizer extends UI3dComponent {
     @Override
     protected void onDraw(UI ui, PGraphics pg) {
         int[] colors = lx.getColors();
-        pg.stroke(0xFF606060);
-        pg.fill(0xFF555555);
+        pg.noStroke();
+        pg.fill(TOWER_COLOR);
 
         for (int i = 0; i < 6; i++) {
             pg.pushMatrix();
             pg.translate(
                 StreetlampModel.ELEMENT_X_SIZE * i,
-                -(TOWER_H / 2 - 5 * StreetlampModel.ELEMENT_Y_DELTA),
+                -(TOWER_H / 2 - TOP_PLANE),
                 StreetlampModel.ELEMENT_Z_SIZE * (5 - i));
             pg.box(StreetlampModel.ELEMENT_X_SIZE, TOWER_H, StreetlampModel.ELEMENT_Z_SIZE);
             pg.popMatrix();
         }
 
-        pg.noStroke();
         for (LXPoint p : model.points) {
             int c = colors[p.index];
             pg.pushMatrix();
@@ -42,6 +45,16 @@ public class StreetlampVisualizer extends UI3dComponent {
             pg.box(
                 StreetlampModel.ELEMENT_X_SIZE,
                 StreetlampModel.ELEMENT_Y_DELTA,
+                StreetlampModel.ELEMENT_Z_SIZE);
+            pg.fill(TOWER_COLOR);
+            float fillTowerHeight = TOP_PLANE - p.y - StreetlampModel.ELEMENT_Y_DELTA / 2f;
+            pg.translate(
+                0,
+                fillTowerHeight / 2f + StreetlampModel.ELEMENT_Y_DELTA / 2f,
+                0);
+            pg.box(
+                StreetlampModel.ELEMENT_X_SIZE,
+                fillTowerHeight,
                 StreetlampModel.ELEMENT_Z_SIZE);
             pg.popMatrix();
         }
