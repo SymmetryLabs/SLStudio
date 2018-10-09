b54cb3de1d907269a1e534fca417c6de326c9370
Jake Lampack <jlampack@gmail.com>
2018-10-09 09:49:16 -0700

    Tweak to MagicLeapShow mappings

--- src/main/java/com/symmetrylabs/shows/magicleap/MagicLeapShow.java
+++ src/main/java/com/symmetrylabs/shows/magicleap/MagicLeapShow.java
@@ -18,33 +18,31 @@ import com.symmetrylabs.shows.Show;
 public class MagicLeapShow extends CubesShow implements Show {
     public static final String SHOW_NAME = "magicleap";
 
-    static final float globalOffsetX = 0;
+    static final float globalOffsetX = 51;
     static final float globalOffsetY = 0;
-    static final float globalOffsetZ = 0;
+    static final float globalOffsetZ = -25;
 
     static final float globalRotationX = 0;
     static final float globalRotationY = 0;
     static final float globalRotationZ = 0;
 
 
+
     static final TowerConfig[] TOWER_CONFIG = {
 
-        // row 1
-        new TowerConfig(SP*0, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*1, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*2, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*3, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*4, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*5, 0, SP*0, new String[] {"0", "0", "0", "0", "0"}),
+        new TowerConfig(SP*-2, 0, SP*1, 0, -90, 0, new String[] {"5410ecf67aeb", "69", "205", "330", "356"}),
+        new TowerConfig(SP*-1, 0, SP*1, 0, -90, 0, new String[] {"329", "186", "51", "87", "35"}),
+        new TowerConfig(SP*0, 0, SP*1, 0, -90, 0, new String[] {"38", "43", "354", "31", "132"}),
+        new TowerConfig(SP*1, 0, SP*1, 0, 0, 0, new String[] {"39", "9", "174", "14", "50"}),
+        new TowerConfig(SP*2, 0, SP*1, 0, 0, 0, new String[] {"412", "185", "120", "55", "320"}),
+        new TowerConfig(SP*3, 0, SP*1, 0, 0, 0, new String[] {"25", "337", "384", "1", "138"}),
 
-        // row 2
-        new TowerConfig(SP*0, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*1, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*2, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*3, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*4, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-        new TowerConfig(SP*5, 0, SP*1, new String[] {"0", "0", "0", "0", "0"}),
-
+        new TowerConfig(SP*-2, 0, SP*2, 0, -90, 0, new String[] {"191", "22", "68", "94", "11"}),
+        new TowerConfig(SP*-1, 0, SP*2, 0, -90, 0, new String[] {"351", "326", "126", "111", "188"}),
+        new TowerConfig(SP*0, 0, SP*2, 0, -90, 0, new String[] {"105", "157", "98", "21", "197"}),
+        new TowerConfig(SP*1, 0, SP*2, 0, 0, 0, new String[] {"34", "358", "408", "91", "76"}),
+        new TowerConfig(SP*2, 0, SP*2, 0, 0, 0, new String[] {"d880399b0833", "23", "122", "61", "66"}),
+        new TowerConfig(SP*3, 0, SP*2, 0, 0, 0, new String[] {"56", "123", "15", "77", "128"}),
     };
 
     public SLModel buildModel() {
