2604e0d8c35593ec54a455dac1e9f6db59965c7f
Jake Lampack <jlampack@gmail.com>
2018-10-10 21:45:51 -0700

    Add Speckle warp

---
+++ src/main/java/com/symmetrylabs/slstudio/warp/Speckle.java
@@ -0,0 +1,42 @@
+package com.symmetrylabs.slstudio.warp;
+
+import heronarts.lx.LX;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.transform.LXVector;
+import heronarts.lx.warp.LXWarp;
+
+import static com.symmetrylabs.util.MathUtils.*;
+
+
+public class Speckle extends LXWarp {
+
+    private final float RANGE = 50;
+
+    private CompoundParameter amount = new CompoundParameter("amt", .3f);
+
+    public Speckle(LX lx) {
+        super(lx);
+        addParameter(amount);
+    }
+
+    public boolean run(double deltaMs, boolean inputVectorsChanged) {
+        if (inputVectorsChanged || getAndClearParameterChangeDetectedFlag()) {
+            float range = RANGE * amount.getValuef();
+
+            for (int i = 0; i < inputVectors.length; i++) {
+                LXVector iv = inputVectors[i];
+                if (iv == null) {
+                    outputVectors[i] = null;
+                } else {
+                    LXVector ov = new LXVector(iv);
+                    ov.add(random(-range, range),
+                             random(-range, range),
+                             random(-range, range));
+                    outputVectors[i] = ov;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
