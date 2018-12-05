566ec7ecabf771c0651d4453087731cc1c2ca9cc
haldean <me@haldean.org>
2018-12-04 16:21:21 -0800

    just look at all these garden looks

--- src/main/java/com/symmetrylabs/shows/hhgarden/FlowerModel.java
+++ src/main/java/com/symmetrylabs/shows/hhgarden/FlowerModel.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlowerModel extends SLModel {
+    public static final int POINT_COUNT = 10;
+
     public enum Group {
         STAMEN,
         PETAL1,
