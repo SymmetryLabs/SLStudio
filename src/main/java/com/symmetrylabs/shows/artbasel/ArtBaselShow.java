c405d47dc6dfa313c942c52be33537cb0131159a
haldean <me@haldean.org>
2018-12-06 15:55:09 -0800

    addressing support for artbasel

--- src/main/java/com/symmetrylabs/shows/artbasel/ArtBaselShow.java
+++ src/main/java/com/symmetrylabs/shows/artbasel/ArtBaselShow.java
@@ -1,17 +1,17 @@
 package com.symmetrylabs.shows.artbasel;
 
-import java.util.*;
-
 import com.symmetrylabs.shows.cubes.CubesModel;
 import com.symmetrylabs.shows.cubes.CubesShow;
 import com.symmetrylabs.shows.cubes.UICubesMappingPanel;
 import com.symmetrylabs.shows.cubes.UICubesOutputs;
+import com.symmetrylabs.slstudio.SLStudioLX;
 import com.symmetrylabs.slstudio.model.SLModel;
-
 import heronarts.lx.transform.LXTransform;
 import heronarts.p3lx.ui.UI2dScrollContext;
-
-import com.symmetrylabs.slstudio.SLStudioLX;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
 
 public class ArtBaselShow extends CubesShow {
     public static final String SHOW_NAME = "artbasel";
@@ -38,12 +38,39 @@ public class ArtBaselShow extends CubesShow {
     static final float INCHES_PER_METER = 39.3701f;
     static final float BALCONY_RADIUS = INCHES_PER_METER * 5f;
 
+    private static final File ID_FILE = new File("shows/artbasel/cube-ids.txt");
+
+    private LinkedList<String[]> loadIds() {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(ID_FILE.toPath());
+        } catch (IOException e) {
+            System.err.println(String.format("couldn't read ID file: %s", e.getMessage()));
+            return null;
+        }
+        LinkedList<String[]> res = new LinkedList<>();
+        for (String line : lines) {
+            line = line.trim();
+            if (line.startsWith("#") || line.length() == 0) {
+                continue;
+            }
+            String[] lineBits = line.split(" ");
+            if (lineBits.length != 2) {
+                System.err.println(String.format("bad format on line: '%s'", line));
+                continue;
+            }
+            res.addLast(lineBits);
+        }
+        return res;
+    }
+
     public SLModel buildModel() {
         // Any global transforms
         LXTransform transform = new LXTransform();
 
         List<CubesModel.Tower> towers = new ArrayList<>();
         List<CubesModel.Cube> allCubes = new ArrayList<>();
+        LinkedList<String[]> ids = loadIds();
 
         for (int t = 0; allCubes.size() < 60; t++) {
             int cubeCount = t % 2 == 0 ? 4 : 3;
@@ -53,9 +80,20 @@ public class ArtBaselShow extends CubesShow {
             float z = BALCONY_RADIUS * (float) Math.sin(theta);
             transform.translate(x, cubeCount == 3 ? CUBE_HEIGHT / 2 : 0, z);
             transform.rotateY(theta);
+
             List<CubesModel.Cube> towerCubes = new ArrayList<>();
             for (int i = 0; i < cubeCount; i++) {
-                CubesModel.Cube c = new CubesModel.Cube("ABC", 0, SP * i, 0, 0, 0, 0, transform, CubesModel.Cube.Type.LARGE_DOUBLE);
+                String[] idPair;
+                if (ids.isEmpty()) {
+                    idPair = new String[] {
+                        String.format("UNMAPPED-%02d-A", allCubes.size() + 1),
+                        String.format("UNMAPPED-%02d-B", allCubes.size() + 1),
+                    };
+                } else {
+                    idPair = ids.removeFirst();
+                }
+                CubesModel.Cube c = new CubesModel.DoubleControllerCube(
+                    idPair[0], idPair[1], 0, SP * i, 0, 0, 0, 0, transform);
                 towerCubes.add(c);
                 allCubes.add(c);
             }
