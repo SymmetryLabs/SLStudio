b40dceba90217f3a93c9f836a879ad07ce4eb481
haldean <me@haldean.org>
2018-12-05 17:20:26 -0800

    new office cube mapping

--- src/main/java/com/symmetrylabs/shows/office/OfficeShow.java
+++ src/main/java/com/symmetrylabs/shows/office/OfficeShow.java
@@ -24,14 +24,21 @@ public class OfficeShow extends CubesShow implements Show {
     static final float globalRotationX = 0;
     static final float globalRotationY = 180;
     static final float globalRotationZ = 0;
-    static final float cuberotation = -45-45;
+    static final float cuberotation = 135;
+
+    static final float diag = (float) Math.sqrt(2) * SP;
 
     static final TowerConfig[] TOWER_CONFIG = {
-        new TowerConfig(0, 0, 0, 0, cuberotation, 0, new String[] { "47", "6", "418", "1118"}),
-        new TowerConfig(SP*1 , 0, 0, 0, cuberotation, 0, new String[] { "106", "390" }),
-        new TowerConfig(SP*1, 0, SP*1, 0, cuberotation, 0, new String[] {"172", "1117", "398", "54"}),
-        new TowerConfig(SP*2, 0, SP*1, 0, cuberotation, 0, new String[] { "175", "422"}),
-        new TowerConfig(SP*2, 0, SP*2, 0, cuberotation, 0, new String[] { "62", "1119", "192", "141"}),
+        new TowerConfig(diag * 0,    0, 0,           0, cuberotation, 0, new String[] { "47", "141", "174", "1118" }),
+        new TowerConfig(diag * 1,    0, 0,           0, cuberotation, 0, new String[] { "123", "132", "11", "68" }),
+        new TowerConfig(diag * 2,    0, 0,           0, cuberotation, 0, new String[] { "172", "1117", "398", "1151" }),
+        new TowerConfig(diag * -.5f, 0, diag * .5f,  0, cuberotation, 0, new String[] { "420", "211" }),
+        new TowerConfig(diag * .5f,  0, diag * .5f,  0, cuberotation, 0, new String[] { "408", "157" }),
+        new TowerConfig(diag * 1.5f, 0, diag * .5f,  0, cuberotation, 0, new String[] { "22", "198" }),
+        new TowerConfig(diag * 2.5f, 0, diag * .5f,  0, cuberotation, 0, new String[] { "86", "314" }),
+        new TowerConfig(diag * 0,    0, diag * 1.5f, 0, cuberotation, 0, new String[] { "34" }),
+        new TowerConfig(diag * 1,    0, diag * 1.5f, 0, cuberotation, 0, new String[] { "32" }),
+        new TowerConfig(diag * 2,    0, diag * 1.5f, 0, cuberotation, 0, new String[] { "51" }),
     };
 
     public SLModel buildModel() {
