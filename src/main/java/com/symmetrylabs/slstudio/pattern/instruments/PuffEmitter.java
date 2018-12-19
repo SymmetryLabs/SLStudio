651f554d0311f8c720ec2580af39c49bf908876c
Ka-Ping Yee <zestyping@gmail.com>
2018-12-19 02:38:47 -1000

    Turn a Puff into a splat.

--- src/main/java/com/symmetrylabs/slstudio/pattern/instruments/PuffEmitter.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/instruments/PuffEmitter.java
@@ -1,6 +1,7 @@
 package com.symmetrylabs.slstudio.pattern.instruments;
 
-import java.util.ArrayList;
+import com.symmetrylabs.util.NoiseUtils;
+
 import java.util.List;
 
 import heronarts.lx.PolyBuffer;
@@ -17,46 +18,63 @@ public class PuffEmitter implements Emitter {
             new LXVector(paramSet.getPoint(pitch, MarkUtils.randomXyDisc())),
             paramSet.getSize(intensity),
             paramSet.getColor(MarkUtils.randomVariation()),
+            paramSet.getOrient(),
+            1/(1 + paramSet.getRate() * 4),
             paramSet.getDecaySec()
         );
     }
 
     class Puff extends AttackDecayMark {
         public LXVector center;
-        public double radius;
+        public float radius;
         public long color;
-        public int[] indexes;
-        public double[] intensities;
+        public float irregularity;
 
         protected LXModel model;
+        protected List<LXPoint> points;
 
-        public Puff(LXVector center, double radius, long color, double decaySec) {
+        protected double growSec;
+        protected float scale;
+
+        public Puff(LXVector center, double radius, long color, double irregularity, double growSec, double decaySec) {
             super(0, decaySec);
 
             this.center = center;
-            this.radius = radius;
+            this.radius = (float) radius;
             this.color = color;
+            this.irregularity = (float) irregularity;
+            this.growSec = growSec;
+            this.scale = 0;
+        }
+
+        public void advance(double deltaSec, double intensity, boolean sustain) {
+            super.advance(deltaSec, intensity, sustain);
+            if (sustain) {
+                scale += deltaSec / growSec;
+                if (scale > 1) scale = 1;
+            }
         }
 
         public void render(LXModel model, PolyBuffer buffer) {
             if (model != this.model) {
                 this.model = model;
-                List<LXPoint> points = MarkUtils.getAllPointsWithin(model, center, radius);
-                indexes = new int[points.size()];
-                intensities = new double[points.size()];
-                for (int i = 0; i < points.size(); i++) {
-                    LXPoint p = points.get(i);
-                    indexes[i] = p.index;
-                    double dx = center.x - p.x;
-                    double dy = center.y - p.y;
-                    double dz = center.z - p.z;
-                    intensities[i] = 1 - (dx*dx + dy*dy + dz*dz)/(radius*radius);
-                }
+                points = MarkUtils.getAllPointsWithin(model, center, radius * 2);
             }
 
             long[] colors = (long[]) buffer.getArray(RGB16);
-            for (int i = 0; i < indexes.length; i++) {
-                MarkUtils.addColor(colors, indexes[i], color, intensities[i] * amplitude);
+            float size = radius * scale;
+            for (LXPoint p : points) {
+                float dx = p.x - center.x;
+                float dy = p.y - center.y;
+                float dz = p.z - center.z;
+                float dist = (float) Math.sqrt(dx*dx + dy*dy + dz*dz)/size;
+                if (irregularity > 0) {
+                    float noise = NoiseUtils.noise(center.x + dx / size, center.y + dy / size, center.z + dz / size);
+                    dist += irregularity * (2 * noise - 1);
+                }
+                if (dist < 1) {
+                    MarkUtils.addColor(colors, p.index, color, amplitude);
+                }
             }
             buffer.markModified(RGB16);
         }
