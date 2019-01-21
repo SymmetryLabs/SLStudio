71f747094ee156905402aa9393b8fec616fb076b
haldean <haldean@symmetrylabs.com>
2019-01-21 13:33:58 -0800

    switch to single prebuilt dir with prebuilt libs

--- src/main/java/com/symmetrylabs/slstudio/ui/gdx/Lwjgl3Launcher.java
+++ src/main/java/com/symmetrylabs/slstudio/ui/gdx/Lwjgl3Launcher.java
@@ -1,10 +1,24 @@
 package com.symmetrylabs.slstudio.ui.gdx;
 
+import java.nio.file.Paths;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.lwjgl.system.Configuration;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
+    public static final String PREBUILT_DIR =
+        Paths.get(System.getProperty("user.dir"), "libs").toString();
+
+    static {
+        /* we want to use our shipped prebuilts for everything, and LWJGL will skip
+             extracting the stuff from its lwjgl-natives JAR if it finds the equivalent
+             dylib in its extract path already. We extract these into a known location so that
+           we can link against the same copy of these dynamic libraries when we
+           build slimgui. */
+        Configuration.SHARED_LIBRARY_EXTRACT_PATH.set(PREBUILT_DIR);
+    }
+
     public static void main(String[] args) {
         createApplication();
     }
