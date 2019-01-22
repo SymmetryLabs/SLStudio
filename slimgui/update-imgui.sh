72725c6666c33da86e5e5504ba9a72b9be9def05
haldean <me@haldean.org>
2019-01-21 16:11:40 -0800

    fix update-imgui scripts to pull in opengl3 imgui source

--- slimgui/update-imgui.sh
+++ slimgui/update-imgui.sh
@@ -5,5 +5,5 @@ cp /tmp/imgui/*.cpp src/main/cpp/
 cp /tmp/imgui/*.h src/main/cpp/
 cp /tmp/imgui/examples/imgui_impl_glfw.cpp src/main/cpp/
 cp /tmp/imgui/examples/imgui_impl_glfw.h src/main/cpp/
-cp /tmp/imgui/examples/imgui_impl_opengl2.cpp src/main/cpp/
-cp /tmp/imgui/examples/imgui_impl_opengl2.h src/main/cpp/
+cp /tmp/imgui/examples/imgui_impl_opengl3.cpp src/main/cpp/
+cp /tmp/imgui/examples/imgui_impl_opengl3.h src/main/cpp/
