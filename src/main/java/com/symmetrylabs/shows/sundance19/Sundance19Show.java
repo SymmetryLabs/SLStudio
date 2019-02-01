760b149adbb5684e84e9e6e9807f6bbbdfae843c
Carlos Alvarez <cmalh@users.noreply.github.com>
2019-01-31 23:35:48 -0700

    Sundance projects

--- src/main/java/com/symmetrylabs/shows/sundance19/Sundance19Show.java
+++ src/main/java/com/symmetrylabs/shows/sundance19/Sundance19Show.java
@@ -15,7 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.*;
-
+ 
 public class Sundance19Show extends CubesShow implements HasWorkspace {
     public static final String SHOW_NAME = "sundance19";
 
@@ -50,125 +50,128 @@ public class Sundance19Show extends CubesShow implements HasWorkspace {
 //TOWER 1
 
             new TowerConfig(SP * 0, SP * 0, SP * 0, new String[][]{
-                new String[] {"757", "756"},
-                new String[] {"1096", "1095"},
-                new String[] {"727", "726"},
-                new String[] {"945", "901"},
+                new String[] {"1046", "1045"},
+                new String[] {"954", "959"},
+                new String[] {"865", "864"},
             }),
 
             new TowerConfig(SP * 1.25f, SP * 0, SP * 0, new String[][]{
-                new String[] {"944", "947"},
-                new String[] {"641", "978"},
-                new String[] {"604", "1087"},
-                new String[] {"960", "974"},
+                new String[] {"767", "766"},
+                new String[] {"775", "774"},
+                new String[] {"1030", "1040"},
+                new String[] {"972", "1081"},
             }),
 
             new TowerConfig(SP * 2.5f, SP * 0, SP * 0, new String[][]{
-                new String[] {"713", "712"},
-                new String[] {"919", "910"},
-                new String[] {"637", "1057"},
-                new String[] {"771", "770"},
+                new String[] {"967", "964"},
+                new String[] {"968", "976"},
+                new String[] {"446", "596"},
+                new String[] {"551", "550"},
+                new String[] {"669", "668"},
             }),
 
             new TowerConfig(SP * 3.75f, SP * 0, SP * 0, new String[][]{
-                new String[] {"654", "1083"},
-                new String[] {"529", "528"},
-                new String[] {"516", "1101"},
-                new String[] {"455", "454"},
+                new String[] {"567", "566"},
+                new String[] {"873", "872"},
+                new String[] {"849", "848"},
+                new String[] {"911", "1063"},
             }),
 
                 new TowerConfig(SP * 5, SP * 0, SP * 0, new String[][]{
-                new String[] {"741", "740"},
-                new String[] {"1041", "1042"},
-                new String[] {"1027", "612"},
-                new String[] {"773", "772"},
+                new String[] {"893", "894"},
+                new String[] {"897", "885"},
+                new String[] {"821", "443"},
             }),
 
 
 ////////////////////////// ROW 2
-                new TowerConfig(-SP * .5f, SP * 0, -SP * 1, new String[][]{
-                new String[] {"876", "520"},
-                new String[] {"916", "917"},
-                new String[] {"905", "955"},
-            }),
+            //     new TowerConfig(-SP * .5f, SP * 0, -SP * 1, new String[][]{
+            //     new String[] {"876", "520"},
+            //     new String[] {"916", "917"},
+            //     new String[] {"905", "955"},
+            // }),
 
                 new TowerConfig(SP * .75f, SP * 0, -SP * 1, new String[][]{
-                new String[] {"1039", "1098"},
-                new String[] {"924", "923"},
-                new String[] {"595", "804"},
+                new String[] {"366", "702"},
+                new String[] {"809", "808"},
+                new String[] {"583", "582"},
             }),
 
                 new TowerConfig(SP * 2, SP * 0, -SP * 1, new String[][]{
-                new String[] {"957", "956"},
-                new String[] {"1067", "1066"},
-                new String[] {"481", "480"},
+                new String[] {"591", "1056"},
+                new String[] {"733", "732"},
+                new String[] {"875", "874"},
+                new String[] {"1072", "1073"},
             }),
 
                 new TowerConfig(SP * 3.25f, SP * 0, -SP * 1, new String[][]{
-                new String[] {"926", "998"},
-                new String[] {"906", "912"},
-                new String[] {"221", "222"},    ////NO NUMBER CUBES NEED ID's
+                new String[] {"778", "970"},
+                new String[] {"971", "994"},
+                new String[] {"1070", "1097"},
+                new String[] {"602", "603"},    ////NO NUMBER CUBES NEED ID's
             }),
 
                 new TowerConfig(SP * 4.5f, SP * 0, -SP * 1, new String[][]{
-                new String[] {"1009", "1008"},
-                new String[] {"935", "999"},
-                new String[] {"997", "996"},
+                new String[] {"531", "983"},
+                new String[] {"498", "881"},
+                new String[] {"794", "795"},
             }),
 
-                new TowerConfig(SP * 5.75f, SP * 0, -SP * 1, new String[][]{
-                new String[] {"929", "995"},
-                new String[] {"923", "918"},
-                new String[] {"1004", "1018"},
-            }),
+            //     new TowerConfig(SP * 5.75f, SP * 0, -SP * 1, new String[][]{
+            //     new String[] {"929", "995"},
+            //     new String[] {"923", "918"},
+            //     new String[] {"1004", "1018"},
+            // }),
 
 
 
 //////////////////// ROW 3 ////////////////////////////////
 
-                new TowerConfig(SP * 0, SP * 0, -SP * 2, new String[][]{
-                new String[] {"1111", "1012"},
-                new String[] {"949", "946"},
-            }),
+            //     new TowerConfig(SP * 0, SP * 0, -SP * 2, new String[][]{
+            //     new String[] {"1111", "1012"},
+            //     new String[] {"949", "946"},
+            // }),
 
-                new TowerConfig(SP * 1.25f, SP * 0, -SP * 2, new String[][]{
-                new String[] {"495", "494"},
-                new String[] {"441", "440"},
+                new TowerConfig(SP * 1.75f, SP * 0, -SP * 2, new String[][]{
+                new String[] {"739", "738"},
+                new String[] {"553", "552"},
             }),
 
                 new TowerConfig(SP * 2.5f, SP * 0, -SP * 2, new String[][]{
-                new String[] {"896", "898"},
-                new String[] {"1104", "1103"},
+                new String[] {"1012", "1039"},
+                new String[] {"977", "102"},
+                new String[] {"647", "646"},
             }),
 
-                new TowerConfig(SP * 3.75f, SP * 0, -SP * 2, new String[][]{
-                new String[] {"679", "678"},
-                new String[] {"707", "706"},
+                new TowerConfig(SP * 3.5f, SP * 0, -SP * 2, new String[][]{
+                new String[] {"451", "450"},
+                new String[] {"1011", "802"},
             }),
 
-                new TowerConfig(SP * 5, SP * 0, -SP * 2, new String[][]{
-                new String[] {"914", "847"},
-                new String[] {"818", "464"},
-            }),
+            //     new TowerConfig(SP * 5, SP * 0, -SP * 2, new String[][]{
+            //     new String[] {"914", "847"},
+            //     new String[] {"818", "464"},
+            // }),
 
 
 
 ///////////////////// ROW 4 /////////////////////////////////////////
 
-                new TowerConfig(SP * .5f, SP * 0, -SP * 3, new String[][]{
-                new String[] {"691", "960"},
+            //     new TowerConfig(SP * .5f, SP * 0, -SP * 3, new String[][]{
+            //     new String[] {"691", "960"},
+            // }),
+
+                new TowerConfig(SP * 2.5f, SP * 0, -SP * 3, new String[][]{
+                new String[] {"852", "853"},
+                new String[] {"449", "448"},
             }),
 
-                new TowerConfig(SP * 1.75f, SP * 0, -SP * 3, new String[][]{
-                new String[] {"825", "821"},
-            }),
-
-                new TowerConfig(SP * 3, SP * 0, -SP * 3, new String[][]{
-                new String[] {"940", "948"},
-            }),
-                new TowerConfig(SP * 4.25f, SP * 0, -SP * 3, new String[][]{
-                new String[] {"1206", "1207"},
-            }),
+                // new TowerConfig(SP * 3, SP * 0, -SP * 3, new String[][]{
+                // new String[] {"940", "948"},
+            // }),
+                // new TowerConfig(SP * 4.25f, SP * 0, -SP * 3, new String[][]{
+                // new String[] {"1206", "1207"},
+            // }),
     };
 
 
@@ -422,7 +425,7 @@ public class Sundance19Show extends CubesShow implements HasWorkspace {
     @Override
     public void setupUi(SLStudioLX lx, SLStudioLX.UI ui) {
         super.setupUi(lx, ui);
-        FrameTimeCrasher.attach(lx, 500);
+        //FrameTimeCrasher.attach(lx, 500);
         workspace = new Workspace(lx, ui, "shows/related");
     }
 
