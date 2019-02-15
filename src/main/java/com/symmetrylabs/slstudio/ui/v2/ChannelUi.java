3c0875be8b33ad857999b16696aba4f66a787370
haldean <me@haldean.org>
2019-02-15 15:35:22 -0800

    show which pattern has midi focus

--- src/main/java/com/symmetrylabs/slstudio/ui/v2/ChannelUi.java
+++ src/main/java/com/symmetrylabs/slstudio/ui/v2/ChannelUi.java
@@ -84,14 +84,11 @@ public class ChannelUi {
 
     public static void draw(LX lx, LXChannel chan, WepUi wepUi) {
         String chanName = chan.getLabel();
-        boolean isFocused = lx.engine.getFocusedChannel() == chan;
 
-        int chanFlags = UI.TREE_FLAG_DEFAULT_OPEN |
-            (isFocused ? UI.TREE_FLAG_SELECTED : 0);
         UI.pushColor(UI.COLOR_HEADER, UI.PURPLE);
         UI.pushColor(UI.COLOR_HEADER_ACTIVE, UI.PURPLE);
         UI.pushColor(UI.COLOR_HEADER_HOVERED, UI.PURPLE_HOVER);
-        UI.selectable(" " + chanName, isFocused, 18);
+        UI.selectable(" " + chanName, lx.engine.getFocusedChannel() == chan, 18);
         UI.popColor(3);
         if (UI.isItemClicked()) {
             lx.engine.addTask(() -> lx.engine.setFocusedChannel(chan));
@@ -146,13 +143,23 @@ public class ChannelUi {
 
             UI.spacing();
 
-            if (active == i) {
+            boolean isActive = active == i;
+            boolean isMidiFocused = pat.controlSurfaceSemaphore.getValue() > 0;
+            if (isActive && isMidiFocused) {
+                UI.pushColor(UI.COLOR_HEADER, UI.PURPLE);
+                UI.pushColor(UI.COLOR_HEADER_ACTIVE, UI.PURPLE);
+                UI.pushColor(UI.COLOR_HEADER_HOVERED, UI.PURPLE_HOVER);
+            } else if (isMidiFocused) {
+                UI.pushColor(UI.COLOR_HEADER, UI.RED);
+                UI.pushColor(UI.COLOR_HEADER_ACTIVE, UI.RED);
+                UI.pushColor(UI.COLOR_HEADER_HOVERED, UI.RED_HOVER);
+            } else if (isActive) {
                 UI.pushColor(UI.COLOR_HEADER, UI.BLUE);
                 UI.pushColor(UI.COLOR_HEADER_ACTIVE, UI.BLUE);
                 UI.pushColor(UI.COLOR_HEADER_HOVERED, UI.BLUE_HOVER);
             }
             UI.CollapseResult section = UI.collapsibleSection(patName, patterns.size() > 1);
-            if (active == i) {
+            if (isActive || isMidiFocused) {
                 UI.popColor(3);
             }
             if (UI.isItemClicked() && UI.isAltDown()) {
