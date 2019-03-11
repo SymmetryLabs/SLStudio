d77f05beeb31aa9301a9a79c9f8af0046ae1fd13
Jake Lampack <jlampack@gmail.com>
2019-03-11 14:30:37 -0700

    Absinthe mapping updates

--- src/main/java/com/symmetrylabs/shows/absinthe/AbsintheShow.java
+++ src/main/java/com/symmetrylabs/shows/absinthe/AbsintheShow.java
@@ -149,8 +149,8 @@ public class AbsintheShow extends TreeShow {
         lx.engine.addLoopTask(new LXLoopTask() {
             @Override
             public void loop(double v) {
-                if (lx.engine.output.brightness.getValuef() > 0.8f) {
-                    lx.engine.output.brightness.setValue(0.8f);
+                if (lx.engine.output.brightness.getValuef() > 0.9f) {
+                    lx.engine.output.brightness.setValue(0.9f);
                 }
                 if (lx.engine.framesPerSecond.getValuef() != 60) {
                     lx.engine.framesPerSecond.setValue(60);
