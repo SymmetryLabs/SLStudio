bde271690dbfccfdd7de20dabecb59a173a86396
Nate Argetsinger <narget@umich.edu>
2019-09-28 20:32:51 -0600

    Java 12

--- src/main/java/com/symmetrylabs/slstudio/Version.java
+++ src/main/java/com/symmetrylabs/slstudio/Version.java
@@ -8,9 +8,9 @@ public class Version {
 	public static final String MAVEN_GROUP = "com.symmetrylabs";
 	public static final String MAVEN_NAME = "SLStudio";
 	public static final String VERSION = "1.0.5";
-	public static final int GIT_REVISION = 3937;
-	public static final String GIT_SHA = "d0fbbe4aff9e9c19023d206bb17bddac5da57fa0";
-	public static final String GIT_DATE = "2019-09-28T16:10:17Z";
-	public static final String BUILD_DATE = "2019-09-28T23:06:36Z";
-	public static final long BUILD_UNIX_TIME = 1569711996602L;
+	public static final int GIT_REVISION = 3938;
+	public static final String GIT_SHA = "d1ce523e5fade065ca97f793237d9e0c0ea108d9";
+	public static final String GIT_DATE = "2019-09-28T23:08:29Z";
+	public static final String BUILD_DATE = "2019-09-29T02:23:04Z";
+	public static final long BUILD_UNIX_TIME = 1569723784053L;
 }
