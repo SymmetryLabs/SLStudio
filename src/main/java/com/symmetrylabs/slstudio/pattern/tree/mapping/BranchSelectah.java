dcaebe79d6270a161936091667cd19cf9cdbfd42
Nate Argetsinger <narget@umich.edu>
2019-11-20 09:34:48 -0500

    pulsing

--- src/main/java/com/symmetrylabs/slstudio/pattern/tree/mapping/BranchSelectah.java
+++ src/main/java/com/symmetrylabs/slstudio/pattern/tree/mapping/BranchSelectah.java
@@ -7,9 +7,11 @@ import heronarts.lx.LX;
 import heronarts.lx.PolyBuffer;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
+import heronarts.lx.modulator.SinLFO;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.LXParameter;
 
 import java.util.List;
 
@@ -20,14 +22,25 @@ public class BranchSelectah extends SLPattern<SLModel> {
     private final TreeModel_v2 model;
     private final DiscreteParameter selectedBrancy;
 
-    private final CompoundParameter bright = new CompoundParameter("bright", 100);
+    private final CompoundParameter bright = new CompoundParameter("bright", 0, 100);
+    private final CompoundParameter pulseFrequency = new CompoundParameter("pulseFrequency", 50, 500);
+
+    final SinLFO x = new SinLFO(0, 100, 200);
 
     public BranchSelectah(LX lx) {
         super(lx);
         this.model = (TreeModel_v2) lx.model;
         this.selectedBrancy = new DiscreteParameter("branch", 0, model.branches.size());
         addParameter(selectedBrancy);
+        addParameter(pulseFrequency);
         addParameter(bright);
+        addModulator(x).trigger();
+    }
+
+    public void onParameterChanged(LXParameter p) {
+        if (p == pulseFrequency) {
+            x.setPeriod(p.getValuef());
+        }
     }
 
     public void run(double deltaMs, PolyBuffer.Space space) {
@@ -37,7 +50,8 @@ public class BranchSelectah extends SLPattern<SLModel> {
         TreeModel_v2.Branch branch = model.branches2.get(selectedBrancy.getValuei());
 
         for (LXPoint p : branch.points){
-            colors[p.index] = LXColor.hsb(0, 0, bright.getValue());
+            colors[p.index] = LXColor.hsb(0, 0, x.getValuef() * bright.getNormalizedf());
+//            colors[p.index] = LXColor.rgb(0, 0, x.getValuef() * bright.getNormalizedf());
         }
         markModified(SRGB8);
     }
