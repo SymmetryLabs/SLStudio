f5032d658700e969770634f606921b9225f8a699
EggMode <frets321@gmail.com>
2020-11-20 17:38:14 -0800

    added charles house show to ysiadsparty

--- src/main/java/com/symmetrylabs/shows/ysiadsparty/YsiadsPartyShow.java
+++ src/main/java/com/symmetrylabs/shows/ysiadsparty/YsiadsPartyShow.java
@@ -26,21 +26,54 @@ public class YsiadsPartyShow extends CubesShow implements Show {
     static final float globalRotationY = 0;
     static final float globalRotationZ = 0;
     static final float CSP = 32;
+    static final float SP = 26;
 
 
     static final TowerConfig[] TOWER_CONFIG = {
-        //back middle (tallest tower)
-        new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521", "123", "422", "192", "157"}),
-        //back towers going to the right
-        new TowerConfig(CSP*.5f, 0, CSP*-.5f, 0, 45, 0, new String[] { "29", "141", "32", "34"}),
-        new TowerConfig(CSP*1, 0, CSP*-1, 0, 45, 0, new String[] { "86", "68", "174",}),
-        new TowerConfig(CSP*1.5f, 0, CSP*-1.5f, 0, 45, 0, new String[] { "43", "25"}),
-        //back towers going to the left
-        new TowerConfig(CSP*-.5f, 0, CSP*-.5f, 0, 45, 0, new String[] { "128", "113", "63", "51"}),
-        new TowerConfig(CSP*-1, 0, CSP*-1, 0, 45, 0, new String[] { "132", "22", "1151",}),
-        //two towers in the middle going to the right
-        new TowerConfig(CSP*0, 0, CSP*-1, 0, 45, 0, new String[] { "1117", "172", "211",}),
-        new TowerConfig(CSP*.5f, 0, CSP*-1.5f, 0, 45, 0, new String[] { "314", "408"}),
+        //Left Cubes bottom to top
+        new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] {"412"}),
+        new TowerConfig(CSP*.25f, 0, CSP*.75f, 0, 45, 0, new String[] { "62"}),
+
+        new TowerConfig(CSP*-.25f, SP * 1, CSP*-.25f, 0, 45, 0, new String[] { "188"}),
+        new TowerConfig(CSP*-.4f, SP*1, CSP*.75f, 0, 45, 0, new String[] {"76"}),
+        new TowerConfig(CSP*.5f, SP*1, SP*1.1f, 0, 45, 0, new String[] {"5410ecf67aeb"}),
+
+        new TowerConfig(CSP*(.5f+.25f), SP*2, SP*(1.1f+.25f), 0, 45, 0, new String[] {"1119"}),
+
+        // //Center Cubes
+        new TowerConfig(CSP*(.5f+.25f+.75f), SP*2.8f, SP*(1.1f+.25f+.33f), 0, 45, 0, new String[] {"1"}),
+        new TowerConfig(CSP*(.5f+.25f+.75f-.75f), SP*(3.8f-1f), SP*(1.1f+.25f+.33f), 90, 90, 90, new String[] {"128"}),
+
+        // //Right Cubes top to bottom
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f), SP*2, SP*(1.1f+.25f), 0, 45, 0, new String[] {"205"}),
+
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f), SP*1, SP*1.1f, 0, 45, 0, new String[] {"69"}),
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f), SP*1, CSP*(.75f), 0, 45, 0, new String[] {"61"}),
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f-.15f), SP*1, CSP*-.25f, 0, 45, 0, new String[] {"38"}),
+
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f-.15f-.5f), SP*0, CSP*.75f, 0, 45, 0, new String[] {"31"}),
+        new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f-.15f-.5f+.25f), SP*0, 0, 0, 0, 0, new String[] {"123"}),
+
+
+        // new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f+.15f-1f), SP*0, CSP*.75f, 0, 45, 0, new String[] {"0"}),
+        // new TowerConfig(CSP*(.5f+.25f+.75f+.75f+.25f+.9f+.15f-1f-.25f), SP*0, 0, 0, 45, 0, new String[] {"0"}),
+
+        // new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521"}),
+        // new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521"}),
+        // new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521"}),
+        // new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521"}),
+        // new TowerConfig(CSP*0, 0, 0, 0, 45, 0, new String[] { "521"}),
+
+        // //back towers going to the right
+        // new TowerConfig(CSP*.5f, 0, CSP*-.5f, 0, 45, 0, new String[] { "29", "141", "32", "34"}),
+        // new TowerConfig(CSP*1, 0, CSP*-1, 0, 45, 0, new String[] { "86", "68", "174",}),
+        // new TowerConfig(CSP*1.5f, 0, CSP*-1.5f, 0, 45, 0, new String[] { "43", "25"}),
+        // //back towers going to the left
+        // new TowerConfig(CSP*-.5f, 0, CSP*-.5f, 0, 45, 0, new String[] { "128", "113", "63", "51"}),
+        // new TowerConfig(CSP*-1, 0, CSP*-1, 0, 45, 0, new String[] { "132", "22", "1151",}),
+        // //two towers in the middle going to the right
+        // new TowerConfig(CSP*0, 0, CSP*-1, 0, 45, 0, new String[] { "1117", "172", "211",}),
+        // new TowerConfig(CSP*.5f, 0, CSP*-1.5f, 0, 45, 0, new String[] { "314", "408"}),
     };
 
     public SLModel buildModel() {
