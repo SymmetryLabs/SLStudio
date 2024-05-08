158a7e22ef8e5699486ae08607d67a6bebb831cc
EggMode <frets321@gmail.com>
2024-05-08 04:22:14 -0500

    added multiple meshes to mapping, something seems off though

--- src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
+++ src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
@@ -1,7 +1,6 @@
 package com.symmetrylabs.shows.mikey;
 
 import com.symmetrylabs.shows.Show;
-import com.symmetrylabs.slstudio.SLStudioLX;
 import com.symmetrylabs.slstudio.model.SLModel;
 import com.symmetrylabs.slstudio.model.Strip;
 import com.symmetrylabs.slstudio.model.StripsModel;
@@ -15,38 +14,47 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
 
 public class MikeyShow implements Show {
     public static final String SHOW_NAME = "mikey";
+    private static final String BASE_PATH = "/Users/Cameron/Desktop/ProgrammingStuff/SLStudio2/SLStudio/src/main/java/com/symmetrylabs/shows/mikey/";
 
     @Override
-public SLModel buildModel() {
-    String jsonContent;
-    try {
-        jsonContent = new String(Files.readAllBytes(Paths.get("/Users/Cameron/Desktop/ProgrammingStuff/SLStudio2/SLStudio/src/main/java/com/symmetrylabs/shows/mikey/type-A_mesh.json")));
-        JSONObject jsonObject = new JSONObject(jsonContent);
-        JSONArray coordinates = jsonObject.getJSONArray("coordinates");
-        double pitch = jsonObject.getDouble("pitch");  // Assuming pitch is in meters or suitable unit
-        List<Strip> strips = new ArrayList<>();
+    public SLModel buildModel() {
+        try {
+            String mainConfigContent = new String(Files.readAllBytes(Paths.get(BASE_PATH + "mainconfig.json")));
+            JSONObject mainConfig = new JSONObject(mainConfigContent);
+            JSONArray fixtures = mainConfig.getJSONObject("model").getJSONArray("fixtures");
 
-        LXTransform t = new LXTransform();
-        for (int i = 0; i < coordinates.length(); i++) {
-            JSONObject coord = coordinates.getJSONObject(i);
-            int row = coord.getInt("row");
-            int col = coord.getInt("col");
+            List<Strip> allStrips = new ArrayList<>();
 
-            t.push();
-            t.translate((float) (col * pitch), (float) (row * pitch), 0);  // Adjusts x and y positions based on pitch
-            Strip.Metrics metrics = new Strip.Metrics(1, pitch); // Assuming each point as a single strip for simplicity
-            strips.add(new Strip(String.format("strip-%d-%d", row, col), metrics, t));
-            t.pop();
+            for (int i = 0; i < fixtures.length(); i++) {
+                JSONObject fixture = fixtures.getJSONObject(i);
+                JSONArray subFixtures = fixture.getJSONArray("fixtures");
+
+                for (int j = 0; j < subFixtures.length(); j++) {
+                    JSONObject subFixture = subFixtures.getJSONObject(j);
+                    String panelType = subFixture.getString("panelType");
+                    double xPos = subFixture.getDouble("xPos");
+                    double yPos = subFixture.getDouble("yPos");
+
+                    String panelContent = new String(Files.readAllBytes(Paths.get(BASE_PATH + panelType + ".json")));
+                    MeshData meshData = parseJson(panelContent, xPos, yPos);
+                    List<Strip> strips = createStrips(meshData);
+                    allStrips.addAll(strips);
+                }
+            }
+
+            return new MikeyModel(allStrips);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        return new MikeyModel(strips);
-    } catch (Exception e) {
-        e.printStackTrace();
-        return null; // Handle errors appropriately
     }
-}
 
     @Override
     public void setupLx(LX lx) {
@@ -56,13 +64,90 @@ public SLModel buildModel() {
         lx.addOutput(pixlite);
     }
 
-    static class MikeyModel extends StripsModel<Strip> {
-        private List<Strip> strips;
+    private MeshData parseJson(String jsonContent, double xPos, double yPos) {
+        JSONObject jsonObject = new JSONObject(jsonContent);
+        JSONArray coordinates = jsonObject.getJSONArray("coordinates");
+        double pitch = jsonObject.getDouble("pitch");
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < coordinates.length(); i++) {
+            JSONObject coord = coordinates.getJSONObject(i);
+            int row = coord.getInt("row");
+            int col = coord.getInt("col");
+            points.add(new Point(row, col));
+        }
+        return new MeshData(points, pitch, xPos, yPos);
+    }
 
+    private List<Strip> createStrips(MeshData meshData) {
+        List<Strip> strips = new ArrayList<>();
+        LXTransform t = new LXTransform();
+        for (Point point : meshData.getPoints()) {
+            int row = point.getRow();
+            int col = point.getCol();
+            t.push();
+            t.translate((float) (col * meshData.getPitch() + meshData.getXPos()), (float) (row * meshData.getPitch() + meshData.getYPos()), 0);
+            Strip.Metrics metrics = new Strip.Metrics(1, meshData.getPitch());
+            strips.add(new Strip(String.format("strip-%d-%d", row, col), metrics, t));
+            t.pop();
+        }
+        return strips;
+    }
+
+    private static class MeshData {
+        private final List<Point> points;
+        private final double pitch;
+        private final double xPos;
+        private final double yPos;
+
+        public MeshData(List<Point> points, double pitch, double xPos, double yPos) {
+            this.points = points;
+            this.pitch = pitch;
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+
+        public List<Point> getPoints() {
+            return points;
+        }
+
+        public double getPitch() {
+            return pitch;
+        }
+
+        public double getXPos() {
+            return xPos;
+        }
+
+        public double getYPos() {
+            return yPos;
+        }
+    }
+
+    private static class Point {
+        private final int row;
+        private final int col;
+
+        public Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+    }
+
+    static class MikeyModel extends StripsModel<Strip> {
         public MikeyModel(List<Strip> strips) {
             super(SHOW_NAME, strips);
-            this.strips = strips;
         }
+    
+
+
 
         public static MikeyModel create() {
             List<Strip> strips = new ArrayList<>();
@@ -85,27 +170,35 @@ public SLModel buildModel() {
     }
 
     static class MikeyPixlite extends SimplePixlite {
-        public MikeyPixlite(LX lx, String ip) {
-            super(lx, ip);
+    public MikeyPixlite(LX lx, String ip) {
+        super(lx, ip);
+    }
+
+    public void configureOutputs(MikeyModel model) {
+        Map<String, PointsGrouping> pointGroupings = new HashMap<>();
+
+        for (int i = 0; i < model.getNumberOfStrips(); i++) {
+            Strip strip = model.getStripByIndex(i);
+            String fixtureId = "1"; // Extract fixture ID from strip label
+
+            // Create a new point grouping for each fixture if not already created
+            PointsGrouping pg = pointGroupings.computeIfAbsent(fixtureId, k -> new PointsGrouping(k));
+            pg.addPoints(strip.getPoints());
         }
 
-        public void configureOutputs(MikeyModel model) {
-            for (int i = 0; i < model.getNumberOfStrips(); i++) {
-                PointsGrouping pg = new PointsGrouping(String.valueOf(i + 1));
-                pg.addPoints(model.getStripByIndex(i).getPoints());
-                addPixliteOutput(pg);
-            }
-        }
+        // Create a new output for each point grouping and add it to the MikeyPixlite instance
+        pointGroupings.values().forEach(this::addPixliteOutput);
+    }
 
-        public SimplePixlite addPixliteOutput(PointsGrouping pointsGrouping) {
-            try {
-                SimplePixliteOutput spo = new SimplePixliteOutput(pointsGrouping);
-                spo.setLogConnections(false);
-                addChild(spo);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return this;
+    public SimplePixlite addPixliteOutput(PointsGrouping pointsGrouping) {
+        try {
+            SimplePixliteOutput spo = new SimplePixliteOutput(pointsGrouping);
+            spo.setLogConnections(false);
+            addChild(spo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this;
         }
     }
 }
