2d2efb4c662b10bfc338e1068abb493d1ef7289d
EggMode <frets321@gmail.com>
2024-05-08 01:52:39 -0500

    first test of paragon mapping

--- src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
+++ src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
@@ -1,18 +1,18 @@
 package com.symmetrylabs.shows.mikey;
 
-import com.google.common.collect.Lists;
 import com.symmetrylabs.shows.Show;
 import com.symmetrylabs.slstudio.SLStudioLX;
-import com.symmetrylabs.slstudio.model.CandyBar;
 import com.symmetrylabs.slstudio.model.SLModel;
 import com.symmetrylabs.slstudio.model.Strip;
 import com.symmetrylabs.slstudio.model.StripsModel;
 import com.symmetrylabs.slstudio.output.SimplePixlite;
 import com.symmetrylabs.slstudio.output.PointsGrouping;
 import heronarts.lx.LX;
-import heronarts.lx.model.LXPoint;
-import heronarts.lx.transform.LXMatrix;
 import heronarts.lx.transform.LXTransform;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,66 +20,83 @@ public class MikeyShow implements Show {
     public static final String SHOW_NAME = "mikey";
 
     @Override
-    public SLModel buildModel() {
-        return MikeyModel.create();
+public SLModel buildModel() {
+    String jsonContent;
+    try {
+        jsonContent = new String(Files.readAllBytes(Paths.get("/Users/Cameron/Desktop/ProgrammingStuff/SLStudio2/SLStudio/src/main/java/com/symmetrylabs/shows/mikey/type-A_mesh.json")));
+        JSONObject jsonObject = new JSONObject(jsonContent);
+        JSONArray coordinates = jsonObject.getJSONArray("coordinates");
+        double pitch = jsonObject.getDouble("pitch");  // Assuming pitch is in meters or suitable unit
+        List<Strip> strips = new ArrayList<>();
+
+        LXTransform t = new LXTransform();
+        for (int i = 0; i < coordinates.length(); i++) {
+            JSONObject coord = coordinates.getJSONObject(i);
+            int row = coord.getInt("row");
+            int col = coord.getInt("col");
+
+            t.push();
+            t.translate((float) (col * pitch), (float) (row * pitch), 0);  // Adjusts x and y positions based on pitch
+            Strip.Metrics metrics = new Strip.Metrics(1, pitch); // Assuming each point as a single strip for simplicity
+            strips.add(new Strip(String.format("strip-%d-%d", row, col), metrics, t));
+            t.pop();
+        }
+        return new MikeyModel(strips);
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null; // Handle errors appropriately
     }
+}
 
     @Override
     public void setupLx(LX lx) {
-        MikeyPixlite pixlite = new MikeyPixlite(lx, "10.200.1.100", (MikeyModel) lx.model);
+        MikeyModel model = (MikeyModel) lx.model;
+        MikeyPixlite pixlite = new MikeyPixlite(lx, "10.200.1.100");
+        pixlite.configureOutputs(model);
         lx.addOutput(pixlite);
     }
 
     static class MikeyModel extends StripsModel<Strip> {
+        private List<Strip> strips;
+
         public MikeyModel(List<Strip> strips) {
             super(SHOW_NAME, strips);
+            this.strips = strips;
         }
 
         public static MikeyModel create() {
-            List<Strip> strips = new ArrayList<Strip>();
+            List<Strip> strips = new ArrayList<>();
             LXTransform t = new LXTransform();
-
-            for (int i = 0; i < 10; i++) {
-                t.push();
-                Strip.Metrics metrics = new Strip.Metrics(139*2, 0.75);
-                Strip strip = new Strip(String.format("strip-%d", i), metrics, t);
-                strips.add(strip);
-//                CandyBar bar = new CandyBar();
-                t.pop();
-                t.translate(0, 2, 0);
-            }
+            t.push();
+            Strip.Metrics metrics = new Strip.Metrics(278, 0.75);
+            Strip strip = new Strip("strip-1", metrics, t);
+            strips.add(strip);
+            t.pop();
             return new MikeyModel(strips);
         }
-    }
-    static class MikeyPixlite extends SimplePixlite {
-        public MikeyPixlite(LX lx, String ip, MikeyModel model) {
-            super(lx, ip);
-//            CandyBar bar = new CandyBar();
-//            addPixliteOutput(
-//                new PointsGrouping("1").addPoints(bar.getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("3").addPoints(model.getStripByIndex(0).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("4").addPoints(model.getStripByIndex(1).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("1").addPoints(model.getStripByIndex(2).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("2").addPoints(model.getStripByIndex(3).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("5").addPoints(model.getStripByIndex(4).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("6").addPoints(model.getStripByIndex(5).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("7").addPoints(model.getStripByIndex(6).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("8").addPoints(model.getStripByIndex(7).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("9").addPoints(model.getStripByIndex(8).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("10").addPoints(model.getStripByIndex(9).getPoints()));
+
+        public int getNumberOfStrips() {
+            return strips.size();
         }
 
-        @Override
+        public Strip getStripByIndex(int index) {
+            return strips.get(index);
+        }
+    }
+
+    static class MikeyPixlite extends SimplePixlite {
+        public MikeyPixlite(LX lx, String ip) {
+            super(lx, ip);
+        }
+
+        public void configureOutputs(MikeyModel model) {
+            for (int i = 0; i < model.getNumberOfStrips(); i++) {
+                PointsGrouping pg = new PointsGrouping(String.valueOf(i + 1));
+                pg.addPoints(model.getStripByIndex(i).getPoints());
+                addPixliteOutput(pg);
+            }
+        }
+
         public SimplePixlite addPixliteOutput(PointsGrouping pointsGrouping) {
             try {
                 SimplePixliteOutput spo = new SimplePixliteOutput(pointsGrouping);
@@ -91,35 +108,4 @@ public class MikeyShow implements Show {
             return this;
         }
     }
-
-
-
-//    static class BarConfig {
-//        final String id;
-//        final float x;
-//        final float y;
-//        final float z;
-//        final float rx;
-//        final float ry;
-//        final float rz;
-//        final int channel;
-//
-//        BarConfig(String id, float[] coordinates, float[] rotations) {
-//            this(id, coordinates, rotations, 0);
-//        }
-//
-//        BarConfig(String id, float[] coordinates, float[] rotations, int channel) {
-//            this.id = id;
-//            this.x = coordinates[0];
-//            this.y = coordinates[1];
-//            this.z = coordinates[2];
-//            this.rx = rotations[0];
-//            this.ry = rotations[1];
-//            this.rz = rotations[2];
-//            this.channel = channel;
-//        }
-//    }
-
-
-
 }
