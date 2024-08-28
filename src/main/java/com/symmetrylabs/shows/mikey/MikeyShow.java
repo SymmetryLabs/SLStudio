5193d94851ce822b7c2e8712235e47dd7d972391
EggMode <frets321@gmail.com>
2024-08-27 20:04:52 -0600

    added mapping for the synesthesia camp tower

--- src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
+++ src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
@@ -27,7 +27,7 @@ public class MikeyShow implements Show {
 
     @Override
     public void setupLx(LX lx) {
-        MikeyPixlite pixlite = new MikeyPixlite(lx, "10.200.1.42", (MikeyModel) lx.model);
+        MikeyPixlite pixlite = new MikeyPixlite(lx, "10.0.0.42", (MikeyModel) lx.model);
         lx.addOutput(pixlite);
     }
 
@@ -41,12 +41,13 @@ public class MikeyShow implements Show {
             int spacing = -60;
             int verticalBar = 22;
             int horizontalBar = 24;
+            int barSpacing = 24;
             List<Strip> strips = new ArrayList<Strip>();
             LXTransform t = new LXTransform();
             Strip.Metrics metricsL4W = new Strip.Metrics(verticalBar*4, 2.15f); //strip config
             Strip.Metrics metricsL3W = new Strip.Metrics(verticalBar*3, 2.15f); //strip config
             Strip.Metrics metricsL3WL = new Strip.Metrics(verticalBar*3, 2.05f); //strip config
-            Strip.Metrics metricsL3S = new Strip.Metrics(verticalBar*3, 1.5f); //strip config
+            Strip.Metrics metricsL3S = new Strip.Metrics(60, 1.5f); //strip config
 
             Strip.Metrics metricsL2 = new Strip.Metrics(verticalBar*2, 1.25f); //strip config
             Strip.Metrics metricsL1 = new Strip.Metrics(verticalBar*1, .75f); //strip config
@@ -57,161 +58,60 @@ public class MikeyShow implements Show {
             Strip.Metrics metricsS2 = new Strip.Metrics(horizontalBar*2, 1); //strip config
             Strip.Metrics metricsS1 = new Strip.Metrics(horizontalBar*1, 1); //strip config
 
-            //OUTPUT A1
             t.translate(0, 0, 0);
             t.push();
-            t.rotateZ(45);
-            Strip strip1 = new Strip("1", metricsL4W, t);         //create the first strip
+            t.rotateZ(1.57);
+            Strip strip1 = new Strip("1", metricsL3S, t);    
             strips.add(strip1);  
             t.pop();
 
-            //OUTPUT A2
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar, 0, 0);
-            t.rotateZ(45);
+            t.rotateZ(1.57);
             Strip strip2 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
             strips.add(strip2);
             t.pop();
 
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar, 0, 0);
-            t.rotateZ(45);
-            t.translate(((horizontalBar*3)*1.5f)+15, 0, 0);
-            t.rotateZ(-45+90+45);
-            Strip strip3 = new Strip("1", metricsS1, t);         //create the second strip
-            strips.add(strip3);                                                  //add the first strip to strip array
+            t.rotateZ(1.57);
+            Strip strip3 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            strips.add(strip3);
             t.pop();
 
-            //OUTPUT A3
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar*2, 0, 0);
-            t.rotateZ(45);
-            Strip strip4 = new Strip("1", metricsL10, t);         //create the turn in the first strip
+            t.rotateZ(1.57);
+            Strip strip4 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
             strips.add(strip4);
             t.pop();
 
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar*2, 0, 0);
-            t.rotateZ(45);
-            t.translate((verticalBar*2)+12, 0, 0);
-            t.rotateZ(-45+90+45);
-            Strip strip5 = new Strip("1", metricsS2, t);         //create the second strip
-            strips.add(strip5);                                                  //add the first strip to strip array
+            t.rotateZ(1.57);
+            Strip strip5 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            strips.add(strip5);
             t.pop();
 
-            //OUTPUT A4
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar*3, 0, 0);
-            t.rotateZ(45);
-            Strip strip6 = new Strip("1", metricsL1, t);         //create the turn in the first strip
+            t.rotateZ(1.57);
+            Strip strip6 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
             strips.add(strip6);
             t.pop();
 
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(horizontalBar*3, 0, 0);
-            t.rotateZ(45);
-            t.translate(horizontalBar*1, 0, 0);
-            t.rotateZ(-45+90+45);
-            Strip strip7 = new Strip("1", metricsS3, t);         //create the second strip
-            strips.add(strip7);                                                  //add the first strip to strip array
+            t.rotateZ(1.57);
+            Strip strip7 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            strips.add(strip7);
             t.pop();
 
-            //OUTPUT A5
+            t.translate(barSpacing, 0, 0);
             t.push();
-            t.translate(0, 0, 0);
-            t.rotateZ(0);
-            Strip strip8 = new Strip("1", metricsS3, t);         //create the turn in the first strip
+            t.rotateZ(1.57);
+            Strip strip8 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
             strips.add(strip8);
-            t.pop();
-
-            //OUTPUT B1
-            t.push();
-            t.translate(horizontalBar*4, 0, 0);
-            t.rotateZ(90);
-            Strip strip9 = new Strip("1", metricsL1, t);         //create the turn in the first strip
-            strips.add(strip9);
-            t.pop();
-
-            t.push();
-            t.translate(horizontalBar*4, 0, 0);
-            t.rotateZ(90);
-            t.translate(horizontalBar*1, 0, 0);
-            t.rotateZ(-90);
-            Strip strip10 = new Strip("1", metricsS3, t);         //create the second strip
-            strips.add(strip10);                                                  //add the first strip to strip array
-            t.pop();
-
-            //OUTPUT B2
-            t.push();
-            t.translate(horizontalBar*5, 0, 0);
-            t.rotateZ(90);
-            Strip strip11 = new Strip("1", metricsL10, t);         //create the turn in the first strip
-            strips.add(strip11);
-            t.pop();
-
-            // t.push();
-            // t.translate(horizontalBar*5, 0, 0);
-            // t.rotateZ(90);
-            // t.translate((verticalBar*2)+8, 0, 0);
-            // t.rotateZ(-90);
-            // Strip strip12 = new Strip("1", metricsS2, t);         //create the second strip
-            // strips.add(strip12);                                                  //add the first strip to strip array
-            // t.pop();
-
-            //OUTPUT B3
-            t.push();
-            t.translate(horizontalBar*6, 0, 0);
-            t.rotateZ(90);
-            Strip strip13 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip13);
-            t.pop();
-
-            t.push();
-            t.translate(horizontalBar*6, 0, 0);
-            t.rotateZ(90);
-            t.translate(((horizontalBar*3)*1.5f)+10, 0, 0);
-            t.rotateZ(-90);
-            Strip strip14 = new Strip("1", metricsS2, t);         //create the second strip
-            strips.add(strip14);                                                  //add the first strip to strip array
-            t.pop();
-
-            //OUTPUT B4
-            t.push();
-            t.translate(horizontalBar*7, 0, 0);
-            t.rotateZ(90);
-            Strip strip15 = new Strip("1", metricsL4W, t);         //create the turn in the first strip
-            strips.add(strip15);
-            t.pop();
-
-            //OUTPUT B5
-            t.push();
-            t.translate(horizontalBar*7, 0, 0);
-            t.rotateZ(135.1f);
-            Strip strip16 = new Strip("1", metricsL10, t);         //create the turn in the first strip
-            strips.add(strip16);
-            t.pop();
-
-            //OUTPUT B4
-            t.push();
-            t.translate(horizontalBar*8, 0, 0);
-            t.rotateZ(90);
-            Strip strip17 = new Strip("1", metricsL10, t);         //create the turn in the first strip
-            strips.add(strip17);
-            t.pop();
-
-            //OUTPUT B5
-            t.push();
-            t.translate(horizontalBar*9, 0, 0);
-            t.rotateZ(135.1f);
-            Strip strip18 = new Strip("1", metricsL10, t);         //create the turn in the first strip
-            strips.add(strip18);
-            t.pop();
-            //OUTPUT B5
-            t.push();
-            t.translate(horizontalBar*10, 0, 0);
-            t.rotateZ(135.1f);
-            Strip strip19 = new Strip("1", metricsL10, t);         //create the turn in the first strip
-            strips.add(strip19);
             t.pop();
 
             return new MikeyModel(strips);
@@ -220,38 +120,23 @@ public class MikeyShow implements Show {
     static class MikeyPixlite extends SimplePixlite {
         public MikeyPixlite(LX lx, String ip, MikeyModel model) {
             super(lx, ip);
-            // for (int i = startStrip; i <= endStrip; i++){
-            //     addPixliteOutput(
-            //     new PointsGrouping((i+1)+"").addPoints(model.getStripByIndex(i).getPoints()));
-            // }
-            //A
             addPixliteOutput(
-                new PointsGrouping("4").addPoints(model.getStripByIndex(0).getPoints()));
+                new PointsGrouping("31").addPoints(model.getStripByIndex(0).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("2").addPoints(model.getStripByIndex(1).getPoints()).addPoints(model.getStripByIndex(2).getPoints()));
+                new PointsGrouping("29").addPoints(model.getStripByIndex(1).getPoints()));
+
             addPixliteOutput(
-                new PointsGrouping("3").addPoints(model.getStripByIndex(3).getPoints()).addPoints(model.getStripByIndex(4).getPoints()));
+                new PointsGrouping("22").addPoints(model.getStripByIndex(2).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("6").addPoints(model.getStripByIndex(5).getPoints()).addPoints(model.getStripByIndex(6).getPoints()));
+                new PointsGrouping("21").addPoints(model.getStripByIndex(3).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("5").addPoints(model.getStripByIndex(7).getPoints()));
-            //B
+                new PointsGrouping("19").addPoints(model.getStripByIndex(4).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("16").addPoints(model.getStripByIndex(8).getPoints()).addPoints(model.getStripByIndex(9).getPoints()));
+                new PointsGrouping("20").addPoints(model.getStripByIndex(5).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("7").addPoints(model.getStripByIndex(10).getPoints()).addPoints(model.getStripByIndex(11).getPoints()));
+                new PointsGrouping("28").addPoints(model.getStripByIndex(6).getPoints()));
             addPixliteOutput(
-                new PointsGrouping("14").addPoints(model.getStripByIndex(12).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("9").addPoints(model.getStripByIndex(13).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("10").addPoints(model.getStripByIndex(14).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("11").addPoints(model.getStripByIndex(15).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("15").addPoints(model.getStripByIndex(16).getPoints()));
-            addPixliteOutput(
-                new PointsGrouping("12").addPoints(model.getStripByIndex(17).getPoints()));
+                new PointsGrouping("27").addPoints(model.getStripByIndex(7).getPoints()));
 
         }
 
