31eeeec54b5840468d158934952e64fc35f940b8
EggMode <frets321@gmail.com>
2025-04-27 00:42:25 -0700

    fixed osx not building buy updating java to 17 and updating processing

--- src/main/java/com/symmetrylabs/slstudio/Version.java
+++ src/main/java/com/symmetrylabs/slstudio/Version.java
@@ -8,9 +8,9 @@ public class Version {
 	public static final String MAVEN_GROUP = "com.symmetrylabs";
 	public static final String MAVEN_NAME = "SLStudio";
 	public static final String VERSION = "1.0.5";
-	public static final int GIT_REVISION = 3959;
-	public static final String GIT_SHA = "e05661728a35ba741e4a07366048038e7358f000";
-	public static final String GIT_DATE = "2021-01-16T08:08:40Z";
-	public static final String BUILD_DATE = "2021-04-22T00:29:01Z";
-	public static final long BUILD_UNIX_TIME = 1619051341446L;
+	public static final int GIT_REVISION = 3393;
+	public static final String GIT_SHA = "158631e341b006a7192f3337e973f942d5ba9732";
+	public static final String GIT_DATE = "2023-08-23T01:47:53Z";
+	public static final String BUILD_DATE = "2025-04-27T07:35:01Z";
+	public static final long BUILD_UNIX_TIME = 1745739301516L;
 }
--- src/main/resources/point-vertex.glsl
+++ src/main/resources/point-vertex.glsl
@@ -1,18 +1,15 @@
-#version 330 core
-
 uniform mat4 u_mvp;
 uniform float u_pointSize;
 
-in vec4 a_position;
-in vec4 a_color;
-
-out vec4 v_color;
+attribute vec4 a_position;
+attribute vec4 a_color;
+varying vec4 v_color;
 
 void main()
 {
-        gl_Position =  u_mvp * a_position;
-        /* poor-man's bloom filter: scale each point by its luminance. */
-        float lum = dot(vec4(0.2126, 0.7152, 0.0722, 0), a_color) / sqrt(3);
-        gl_PointSize = u_pointSize * (1 + 3.5 * lum);
-        v_color = a_color;
+    gl_Position =  u_mvp * a_position;
+    /* poor-man's bloom filter: scale each point by its luminance. */
+    float lum = dot(vec4(0.2126, 0.7152, 0.0722, 0), a_color) / sqrt(3.0);
+    gl_PointSize = u_pointSize * (1.0 + 3.5 * lum);
+    v_color = a_color;
 }
