4f43879d7897ad5df7378c6f7e054cc835e465f3
Cameron Nagai <frets321@gmail.com>
2025-04-29 13:10:19 -0700

    got composite show working on windows

--- src/main/java/com/symmetrylabs/slstudio/Version.java
+++ src/main/java/com/symmetrylabs/slstudio/Version.java
@@ -8,9 +8,9 @@ public class Version {
 	public static final String MAVEN_GROUP = "com.symmetrylabs";
 	public static final String MAVEN_NAME = "SLStudio";
 	public static final String VERSION = "1.0.5";
-	public static final int GIT_REVISION = 3959;
-	public static final String GIT_SHA = "e05661728a35ba741e4a07366048038e7358f000";
-	public static final String GIT_DATE = "2021-01-16T08:08:40Z";
-	public static final String BUILD_DATE = "2021-04-22T00:29:01Z";
-	public static final long BUILD_UNIX_TIME = 1619051341446L;
+	public static final int GIT_REVISION = 3396;
+	public static final String GIT_SHA = "31eeeec54b5840468d158934952e64fc35f940b8";
+	public static final String GIT_DATE = "2025-04-27T07:42:25Z";
+	public static final String BUILD_DATE = "2025-04-29T05:07:40Z";
+	public static final long BUILD_UNIX_TIME = 1745903260748L;
 }
