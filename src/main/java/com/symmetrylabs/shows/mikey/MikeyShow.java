dd9d5d85438372561dd6af91f6977794c724d028
EggMode <frets321@gmail.com>
2025-06-08 18:22:07 -0700

    added all the controllers and outputs for cloud room to mikeyshow

--- src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
+++ src/main/java/com/symmetrylabs/shows/mikey/MikeyShow.java
@@ -32,6 +32,27 @@ public class MikeyShow implements Show {
         lx.addOutput(pixlite1);
         MikeyPixlite pixlite2 = new MikeyPixlite(lx, "192.168.0.193", model, 8);      // strips 8-15
         lx.addOutput(pixlite2);
+        //add a total of 12 controllers
+        MikeyPixlite pixlite3 = new MikeyPixlite(lx, "192.168.0.194", model, 16);      // strips 16-23
+        lx.addOutput(pixlite3); 
+        MikeyPixlite pixlite4 = new MikeyPixlite(lx, "192.168.0.195", model, 24);      // strips 24-31
+        lx.addOutput(pixlite4); 
+        MikeyPixlite pixlite5 = new MikeyPixlite(lx, "192.168.0.196", model, 32);      // strips 32-39
+        lx.addOutput(pixlite5); 
+        MikeyPixlite pixlite6 = new MikeyPixlite(lx, "192.168.0.197", model, 40);      // strips 40-47
+        lx.addOutput(pixlite6); 
+        MikeyPixlite pixlite7 = new MikeyPixlite(lx, "192.168.0.198", model, 48);      // strips 48-55
+        lx.addOutput(pixlite7); 
+        MikeyPixlite pixlite8 = new MikeyPixlite(lx, "192.168.0.199", model, 56);      // strips 56-63
+        lx.addOutput(pixlite8); 
+        MikeyPixlite pixlite9 = new MikeyPixlite(lx, "192.168.0.200", model, 64);      // strips 64-71
+        lx.addOutput(pixlite9); 
+        MikeyPixlite pixlite10 = new MikeyPixlite(lx, "192.168.0.201", model, 72);      // strips 72-79
+        lx.addOutput(pixlite10); 
+        MikeyPixlite pixlite11 = new MikeyPixlite(lx, "192.168.0.202", model, 80);      // strips 80-87
+        lx.addOutput(pixlite11); 
+        MikeyPixlite pixlite12 = new MikeyPixlite(lx, "192.168.0.203", model, 88);      // strips 88-95
+        lx.addOutput(pixlite12); 
     }
 
     static class MikeyModel extends StripsModel<Strip> {
@@ -61,117 +82,186 @@ public class MikeyShow implements Show {
             Strip.Metrics metricsS2 = new Strip.Metrics(horizontalBar*2, 1); //strip config
             Strip.Metrics metricsS1 = new Strip.Metrics(horizontalBar*1, 1); //strip config
 
-            t.translate(0, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip1 = new Strip("1", metricsL3S, t);    
-            strips.add(strip1);  
-            t.pop();
+            // t.translate(0, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip1 = new Strip("1", metricsL3S, t);    
+            // strips.add(strip1);  
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip2 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip2);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip2 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip2);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip3 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip3);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip3 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip3);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip4 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip4);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip4 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip4);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip5 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip5);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip5 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip5);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip6 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip6);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip6 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip6);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip7 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip7);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip7 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip7);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip8 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip8);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip8 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip8);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip9 = new Strip("1", metricsL3S, t);    
-            strips.add(strip9);  
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip9 = new Strip("1", metricsL3S, t);    
+            // strips.add(strip9);  
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip10 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip10);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip10 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip10);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip11 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip11);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip11 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip11);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip12 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip12);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip12 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip12);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip13 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip13);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip13 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip13);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip14 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip14);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip14 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip14);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip15 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip15);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip15 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip15);
+            // t.pop();
 
-            t.translate(barSpacing, 0, 0);
-            t.push();
-            t.rotateZ(1.57);
-            Strip strip16 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
-            strips.add(strip16);
-            t.pop();
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip16 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip16);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip17 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip17);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip18 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip18);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip19 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip19);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip20 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip20);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip21 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip21);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip22 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip22);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip23 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip23);
+            // t.pop();
+
+            // t.translate(barSpacing, 0, 0);
+            // t.push();
+            // t.rotateZ(1.57);
+            // Strip strip24 = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+            // strips.add(strip24);
+            // t.pop();
+
+            for (int i = 0; i < 96; i++) {
+                t.translate(barSpacing, 0, 0);
+                t.push();
+                t.rotateZ(1.57);
+                Strip strip = new Strip("1", metricsL3S, t);         //create the turn in the first strip
+                strips.add(strip);
+                t.pop();
+            }
+
+
+            
+
         
             return new MikeyModel(strips);
         }
